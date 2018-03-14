fix mistake in partitioned negative binomial fit: wrong index order; linear fit is working now

impl/tf/negative_binomial/util.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow.contrib.distributions import NegativeBinomial
 
@@ -19,23 +20,35 @@ def fit_partitioned(sample_data, design, optimizable=False) -> (tf.Tensor, tf.Te
     :param optimizable: if true, the returned parameters will be optimizable
     :return: tuple of shape and probabilities for each negative binomial distribution
     """
-    num_parts = len(np.unique(design))
-
-    dyn_parts = tf.dynamic_partition(sample_data, design, num_parts, name="dynamic_parts")
+    
+    design_df = pd.DataFrame(design)
+    design_df["row_nr"] = design_df.index
+    
+    # create DataFrame containing all unique rows and assign a unique index to them
+    unique_rows = np.unique(design, axis=0)
+    unique_rows = pd.DataFrame(unique_rows)
+    unique_rows["idx"] = unique_rows.index
+    # merge the unique row indexes to the design matrix
+    indexed_design = design_df.merge(unique_rows)
+    indexed_design = indexed_design.sort_values(by=['row_nr'])
+    
+    partition_index = list(indexed_design["idx"])
+    
+    dyn_parts = tf.dynamic_partition(sample_data, partition_index, unique_rows.shape[0], name="dynamic_parts")
     params_r = list()
     params_p = list()
     for idx, part in enumerate(dyn_parts):
         dist = fit(part, optimizable=optimizable, name="negbin_%i" % idx)
         params_r.append(dist.total_count)
         params_p.append(dist.probs)
-
+    
     stacked_r = tf.stack(params_r, name="stack_r")
     stacked_p = tf.stack(params_p, name="stack_p")
-
-    stacked_r = tf.gather(stacked_r, design)
-    stacked_p = tf.gather(stacked_p, design)
-
-    return stacked_p, stacked_r
+    
+    stacked_r = tf.gather(stacked_r, partition_index)
+    stacked_p = tf.gather(stacked_p, partition_index)
+    
+    return stacked_r, stacked_p
 
 
 def fit(sample_data, optimizable=False, name="nb-dist") -> NegativeBinomial:
@@ -50,24 +63,24 @@ def fit(sample_data, optimizable=False, name="nb-dist") -> NegativeBinomial:
     """
     with tf.name_scope("fit"):
         (r, p) = fit_mme(sample_data)
-
+        
         if optimizable:
             r = tf.Variable(name="r", initial_value=r, dtype=tf.float32, validate_shape=False)
             # r_var = tf.Variable(tf.zeros(tf.shape(r)), dtype=tf.float32, validate_shape=False, name="r_var")
             #
             # r_assign_op = tf.assign(r_var, r)
-
+        
         # keep mu constant
         mu = tf.reduce_mean(sample_data, axis=0, name="mu")
-
+        
         # p is directly dependent from mu and r
         p = mu / (r + mu)
         p = tf.identity(p, "p")
-
+    
     distribution = NegativeBinomial(total_count=r,
                                     probs=p,
                                     name=name)
-
+    
     return distribution
 
 
@@ -83,7 +96,7 @@ def fit_mme(sample_data, replace_values=None, dtype=None, name="MME") -> (tf.Ten
         """
     if dtype is None:
         dtype = sample_data.dtype
-
+    
     with tf.name_scope(name):
         mean = tf.reduce_mean(sample_data, axis=0, name="mean")
         variance = tf.reduce_mean(tf.square(sample_data - mean),
@@ -91,14 +104,14 @@ def fit_mme(sample_data, replace_values=None, dtype=None, name="MME") -> (tf.Ten
                                   name="variance")
         if replace_values is None:
             replace_values = tf.fill(tf.shape(variance), tf.constant(math.inf, dtype=dtype), name="inf_constant")
-
+        
         r_by_mean = tf.where(tf.less(mean, variance),
                              mean / (variance - mean),
                              replace_values)
         r = r_by_mean * mean
         r = tf.identity(r, "r")
-
+        
         p = 1 / (r_by_mean + 1)
         p = tf.identity(p, "p")
-
+        
         return r, p

interactive.py
@@ -27,35 +27,15 @@ sess = tf.InteractiveSession()
 (real_r, real_p) = sess.run((r, p))
 
 
-def as_design_matrix(design, num_bits=8):
-    def uint2bits(int_ar_in, num_bits):
-        """ convert (numpyarray of uint => array of Nbits bits) for many bits in parallel"""
-        in_size__t = int_ar_in.shape
-        in_flat = int_ar_in.flatten()
-        out_num_bit = np.zeros((len(in_flat), num_bits))
-        for i_bits in range(num_bits):
-            out_num_bit[:, i_bits] = (in_flat >> i_bits) & 1
-        out_num_bit = out_num_bit.reshape(in_size__t + (num_bits,))
-        return out_num_bit
-
-    # design_bin = np.unpackbits(np.expand_dims(design, 1).astype(np.uint8), axis=1)
-    design_bin = uint2bits(design.astype(np.uint32), 8)
-    mask = np.all(design_bin == 0, axis=0)
-    design_bin = design_bin[:, ~mask]
-    # design_bin = np.unique(design_bin, axis=0)
-    # design_bin = np.unique(design_bin, axis=1)
-
-    # add bias column
-    n, m = design_bin.shape  # for generality
-    ones = np.ones((n, 1))
-    design_bin = np.hstack((ones, design_bin))
-
-    return design_bin
+x = tf.constant(design, name="X")
+y = r[:,0]
 
 
-design_bin = as_design_matrix(design)
-np.unique(design_bin, axis=0)
+solve = tf.matrix_solve_ls(design, tf.log(tf.expand_dims(r[:,0], 1)))
 
-tf.matrix_solve_ls
+a = sess.run(solve)
 
-from patsy import dmatrix
+xt= tf.transpose(x, name="Xt")
+xtx = tf.matmul(xt, x, name="XtX")
+xtx_inv = tf.matrix_inverse(xtx, name="xtx_inv")
+xty = tf.matmul(xt, tf.expand_dims(y, 1))