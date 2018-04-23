2b1636c8cd036597b11f4a6eacbe7e12ae9a9c57
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2018-04-23 12:20:17 +0200

    improved utils

--- utils/random.py
+++ utils/random.py
@@ -1,40 +1,93 @@
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.contrib.distributions import NegativeBinomial
+import impl.tf.negative_binomial.util as nb_utils
 
 from tensorflow.contrib import eager as tfe
 
 
-def negative_binomial(r, mu, as_numpy_array=True):
-    # ugly hack using tensorflow, since parametrisation with `p`
-    # does not work with np.random.negative_binomial
+class NegativeBinomial:
+    mean: np.ndarray
+    variance: np.ndarray
+    p: np.ndarray
+    r: np.ndarray
 
-    retVal = None
-    with tf.Graph().as_default():
-        r = tf.convert_to_tensor(r, dtype=tf.float64)
-        mu = tf.convert_to_tensor(mu, dtype=tf.float64)
-        r = tf.cast(r, tf.float64)
-        mu = tf.cast(mu, tf.float64)
+    def __init__(self, r=None, variance=None, p=None, mean=None, name="NegativeBinomial"):
+        if r is not None:
+            if variance is not None:
+                raise ValueError("Must pass either shape 'r' or variance, but not both")
 
-        p = mu / (r + mu)
+            if p is not None:
+                if mean is not None:
+                    raise ValueError("Must pass either probs or means, but not both")
 
-        dist = NegativeBinomial(total_count=r, probs=p)
+                mean = p * r / (1 - p)
+                variance = mean / (1 - p)
 
-        retVal = tf.squeeze(dist.sample(1))
+            elif mean is not None:
+                if p is not None:
+                    raise ValueError("Must pass either probs or means, but not both")
 
-        # run sampling session
-        if not tfe.in_eager_mode():
-            with tf.Session() as sess:
-                retVal = sess.run(retVal)
+                p = mean / (r + mean)
+                variance = mean + (np.square(mean) / r)
 
-    # random_data = np.random.negative_binomial(
-    #     self.r,
-    #     self.p,
-    # )
-    # return random_data
+            else:
+                raise ValueError("Must pass probs or means")
 
-    if as_numpy_array:
-        retVal = np.asarray(retVal)
+        elif variance is not None:
+            if r is not None:
+                raise ValueError("Must pass either shape 'r' or variance, but not both")
 
-    return retVal
+            if p is not None:
+                if mean is not None:
+                    raise ValueError("Must pass either probs or means, but not both")
+
+                mean = variance * (1 - p)
+                r = mean / (variance - mean)
+
+            elif mean is not None:
+                if p is not None:
+                    raise ValueError("Must pass either probs or means, but not both")
+
+                p = 1 - (mean / variance)
+                r = mean * mean / (variance - mean)
+            else:
+                raise ValueError("Must pass probs or means")
+        else:
+            raise ValueError("Must pass shape 'r' or variance")
+
+        self.mean = mean
+        self.variance = variance
+        self.r = r
+        self.p = p
+
+    def sample(self, size=1, as_numpy_array=True):
+        # ugly hack using tensorflow, since parametrisation with `p`
+        # does not work with np.random.negative_binomial
+
+        retVal = None
+        with tf.Graph().as_default():
+            mean = tf.convert_to_tensor(self.mean, dtype=tf.float64)
+            variance = tf.convert_to_tensor(self.variance, dtype=tf.float64)
+            mu = tf.cast(mean, tf.float64)
+            variance = tf.cast(variance, tf.float64)
+
+            dist = nb_utils.NegativeBinomial(mean=mean, variance=variance)
+
+            retVal = tf.squeeze(dist.sample(size))
+
+            # run sampling session
+            if not tfe.in_eager_mode():
+                with tf.Session() as sess:
+                    retVal = sess.run(retVal)
+
+        # random_data = np.random.negative_binomial(
+        #     self.r,
+        #     self.p,
+        # )
+        # return random_data
+
+        if as_numpy_array:
+            retVal = np.asarray(retVal)
+
+        return retVal
--- utils/stats.py
+++ utils/stats.py
@@ -1,90 +1,72 @@
-import tensorflow as tf
+import numpy as np
 
 
-def normalize(measure: tf.Tensor, data: tf.Tensor, name="normalize") -> tf.Tensor:
+def normalize(measure: np.ndarray, data: np.ndarray) -> np.ndarray:
     """
     Normalize measure (e.g. `RMSD` or `MAE`) with the range of `data`
 
     :param measure: the measure which should be normalized
     :param data: Tensor representing the data by which the measure should be normalized
-    :param name: name of this operation
     :return: \frac{RMSD}{max(data) - min(data)}
     """
-    with tf.name_scope(name):
-        retval = measure / (tf.maximum(data) - tf.minimum(data))
-    return retval
+    return measure / (np.max(data) - np.min(data))
 
 
-def rmsd(estim: tf.Tensor, obs: tf.Tensor, axis=None, name="RMSD") -> tf.Tensor:
+def rmsd(estim: np.ndarray, obs: np.ndarray, axis=None) -> np.ndarray:
     """
     Calculate the root of the mean squared deviation between the estimated and the observed data
 
     :param estim: Tensor representing the estimated data
     :param obs: Tensor representing the observed data
     :param axis: axis to reduce
-    :param name: name of this operation
     :return: \sqrt{mean{(estim - obs)^2}}
     """
-    with tf.name_scope(name):
-        retval = tf.sqrt(tf.reduce_mean(tf.squared_difference(estim, obs), axis=axis))
-    return retval
+    return np.sqrt(np.mean(np.square(estim - obs), axis=axis))
 
 
-def mae(estim: tf.Tensor, obs: tf.Tensor, axis=None, name="MAE") -> tf.Tensor:
+def mae(estim: np.ndarray, obs: np.ndarray, axis=None) -> np.ndarray:
     """
     Calculate the mean absolute error between the estimated weights `b` and the true `b`
 
     :param estim: Tensor representing the estimated data
     :param obs: Tensor representing the observed data
     :param axis: axis to reduce
-    :param name: name of this operation
     :return: mean{|estim - obs|}
     """
-    with tf.name_scope(name):
-        retval = tf.reduce_mean(tf.abs(estim - obs), axis=axis)
-    return retval
+    return np.mean(np.abs(estim - obs), axis=axis)
 
 
-def normalized_rmsd(estim: tf.Tensor, obs: tf.Tensor, axis=None, name="NRMSD") -> tf.Tensor:
+def normalized_rmsd(estim: np.ndarray, obs: np.ndarray, axis=None) -> np.ndarray:
     """
     Calculate the normalized RMSD between estimated and observed data
 
     :param estim: Tensor representing the estimated data
     :param obs: Tensor representing the observed data
     :param axis: axis to reduce
-    :param name: name of this operation
     :return: \frac{RMSD}{max(obs) - min(obs)}
     """
-    with tf.name_scope(name):
-        retval = normalize(rmsd(estim, obs, axis=axis), obs)
-    return retval
+    return normalize(rmsd(estim, obs, axis=axis), obs)
 
 
-def normalized_mae(estim: tf.Tensor, obs: tf.Tensor, axis=None, name="NMAE") -> tf.Tensor:
+def normalized_mae(estim: np.ndarray, obs: np.ndarray, axis=None) -> np.ndarray:
     """
     Calculate the normalized MAE between estimated and observed data
 
     :param estim: Tensor representing the estimated data
     :param obs: Tensor representing the observed data
     :param axis: axis to reduce
-    :param name: name of this operation
     :return: \frac{MAE}{max(obs) - min(obs)}
     """
-    with tf.name_scope(name):
-        retval = normalize(mae(estim, obs, axis=axis), obs)
-    return retval
+    return normalize(mae(estim, obs, axis=axis), obs)
 
 
-def mapd(estim: tf.Tensor, obs: tf.Tensor, axis=None, name="MAPD") -> tf.Tensor:
+def mapd(estim: np.ndarray, obs: np.ndarray, axis=None) -> np.ndarray:
     """
     Calculate the mean absolute percentage deviation between the estimated and the observed data
 
     :param estim: ndarray representing the estimated data
     :param obs: ndarray representing the observed data
     :param axis: axis to reduce
-    :param name: name of this operation
     :return: mean{|estim - obs| / obs}
     """
-    with tf.name_scope(name):
-        retval = tf.reduce_mean(tf.abs(estim - obs) / obs, axis=axis)
-    return retval
+    return np.mean(np.abs(estim - obs) / obs, axis=axis)
