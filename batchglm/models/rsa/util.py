improve usability of mixture priors and remove need for manual initialization; fix some bugs in computation; incorporate mixture priors in initialization

batchglm/models/rsa/util.py
@@ -12,7 +12,7 @@ from batchglm.utils.numeric import combine_matrices
 def _mdesc_setup(
         sample_description: Union[np.ndarray, pd.DataFrame],
         formula,
-        groups: Union[str, np.ndarray],
+        num_mixtures: int,
         differing_factors: Union[Tuple[str], List[str]] = ("Intercept",),
         dim_names: Tuple[str, str] = ("design_params", "mixtures", "design_mixture_params")
 ):
@@ -29,7 +29,6 @@ def _mdesc_setup(
     #
     # groups, _ = np.unique(mixture_grouping.astype(str), return_inverse=True)
 
-    num_mixtures = np.size(groups)
     # num_observations = len(sample_description)
     # num_design_params = len(dmat.design_info.column_names)
 
@@ -46,12 +45,94 @@ def _mdesc_setup(
     return dmat, design_tensor
 
 
+def mixture_prior_setup(
+        mixture_grouping: Union[List, np.ndarray],
+        mixture_model_desc: pd.DataFrame,
+) -> pd.DataFrame:
+    """
+    Creates a mixture prior
+
+    :param mixture_grouping: vector of length `num_observations` denoting the group of an observation.
+    :param mixture_model_desc: Pandas DataFrame of shape `(num_groups, mixtures)` containing booleans.
+
+        This table describes the structure of the mixture model:
+        It allows to specify, which of the observations are allowed to be assigned to which mixture.
+        Example:
+
+        ..
+            ```python3
+            pd.DataFrame({
+                "m_norm":  [1, 1, 1],
+                "m_tr1.1": [0, 1, 0],
+                "m_tr1.2": [0, 1, 0],
+                "m_tr2.1": [0, 0, 1],
+                "m_tr2.2": [0, 0, 1],
+            }, index = ["norm", "tr1", "tr2"])
+            ```
+
+                  m_norm  m_tr1.1  m_tr1.2  m_tr2.1  m_tr2.2
+            norm       1        0        0        0        0
+            tr1        1        1        1        0        0
+            tr2        1        0        0        1        1
+
+        In this example, all normal samples are only allowed to occur in the mixture `m_norm`.
+        All `tr1` samples can occur in the mixtures `m_norm`, `m_tr1.1` and `m_tr1.2`.
+        All `tr2` samples can occur in the mixtures `m_norm`, `m_tr2.1` and `m_tr2.2`.
+    :return: Pandas Dataframe of shape `(num_observations, num_mixtures)`
+    """
+    mixture_grouping = np.asarray(mixture_grouping)
+
+    # num_mixtures = pin_mixtures
+    # if pin_mixtures is not None:
+    #     pin_mixtures = np.asarray(pin_mixtures)
+    #     if np.issubdtype(pin_mixtures.dtype, np.integer):
+    #         pinned_assignments = pin_mixtures
+    #     else:
+    #         if np.size(pin_mixtures) == 1:
+    #             pin_mixtures = {pin_mixtures.flatten()[0]}
+    #         else:
+    #             pin_mixtures = set(pin_mixtures.astype(str))
+    #         pinned_assignments = np.where(np.vectorize(lambda x: x in pin_mixtures)(mixture_grouping))
+    #
+    #     pin_mask = np.zeros([num_observations, num_mixtures])
+    #     pin_mask[pinned_assignments] = 1
+    #     pin_mask = pin_mask.astype(bool)
+    #
+    #     mixture_weight_priors = np.where(pin_mask, init_mixture_weights, np.ones_like(init_mixture_weights))
+    # else:
+    #     mixture_weight_priors = None
+
+    if np.issubdtype(mixture_grouping.dtype, np.integer):
+        return mixture_model_desc.iloc[mixture_grouping]
+    else:
+        return mixture_model_desc.loc[mixture_grouping]
+
+
+# def mixture_weight_init(
+#         mixture_priors: pd.DataFrame,
+#         initial_mixture_assignments: pd.Series
+# ):
+#     """
+#     Creates an array of initial mixture weights
+#
+#     :param mixture_priors: Pandas Dataframe of mixture priors. See also :func:mixture_prior_setup
+#     :param initial_mixture_assignments: Pandas Series of length
+#     :return: numpy array `(num_observations, num_mixtures)`
+#     """
+#     bcast_init_assignments = initial_mixture_assignments.loc[mixture_priors.index]
+#
+#     initial_mixture_weights = np.zeros(mixture_priors.shape)
+#     initial_mixture_weights[range(len(mixture_priors)), bcast_init_assignments] = 1
+#
+#     return initial_mixture_weights
+
+
 def mixture_model_setup(
         sample_description: Union[np.ndarray, pd.DataFrame],
         formula_loc,
         formula_scale,
-        mixture_grouping: Union[str, np.ndarray],
-        pin_mixtures: Union[Iterable[str], Iterable[int], None] = None,
+        mixture_model_desc: pd.DataFrame,
+        mixture_grouping: Union[str, Iterable],
         differing_factors_loc: Union[Tuple[str], List[str]] = ("Intercept",),
         differing_factors_scale: Union[Tuple[str], List[str]] = ("Intercept",),
         dim_names_loc: Tuple[str, str] = ("design_loc_params", "mixtures", "design_mixture_loc_params"),
@@ -61,26 +142,42 @@ def mixture_model_setup(
     xr.DataArray,
     patsy.highlevel.DesignMatrix,
     xr.DataArray,
-    np.ndarray,
-    np.ndarray
+    pd.DataFrame
 ]:
     r"""
-    Build a mixture description.
+    Set up a mixture model.
 
     :param sample_description: Some sample description which can be used to build a design matrix
     :param formula_loc: Formula to set up a model design for location
     :param formula_scale: Formula to set up a model design for scale
+    :param mixture_model_desc: Pandas DataFrame of shape `(num_groups, mixtures)` containing booleans.
+
+        This table describes the structure of the mixture model:
+        It allows to specify, which of the observations are allowed to be assigned to which mixture.
+        Example:
+
+        ..
+            ```python3
+            pd.DataFrame({
+                "m_norm":  [1, 1, 1],
+                "m_tr1.1": [0, 1, 0],
+                "m_tr1.2": [0, 1, 0],
+                "m_tr2.1": [0, 0, 1],
+                "m_tr2.2": [0, 0, 1],
+            }, index = ["norm", "tr1", "tr2"])
+            ```
+
+                  m_norm  m_tr1.1  m_tr1.2  m_tr2.1  m_tr2.2
+            norm       1        0        0        0        0
+            tr1        1        1        1        0        0
+            tr2        1        0        0        1        1
+
+        In this example, all normal samples are only allowed to occur in the mixture `m_norm`.
+        All `tr1` samples can occur in the mixtures `m_norm`, `m_tr1.1` and `m_tr1.2`.
+        All `tr2` samples can occur in the mixtures `m_norm`, `m_tr2.1` and `m_tr2.2`.
     :param mixture_grouping: either string pointing to one column in `sample_description` or a list of assignments.
 
-        Describes some (assumed) mixture assignment.
-        The number of mixtures is derived from the number of unique elements in this grouping.
-    :param pin_mixtures:
-        Allows to specify, which of the observations should be assigned permanently to the initial mixture,
-        i.e. are not allowed to change.
-
-        Can be either:
-            - Iterable of strings: Will fix the assignments where `mixture_grouping == anyof(pin_mixtures)`
-            - Iterable of ints: Will fix the assignments at `observations[pin_mixtures]`
+        Describes which observations correspond to which group (i.e. row) in `mixture_model_desc`
     :param differing_factors_loc: Terms of the location formula which are allowed to differ across mixtures
     :param differing_factors_scale: Terms of the scale formula which are allowed to differ across mixtures
     :param dim_names_loc: dimension names; defaults to ("mixtures", "design_params").
@@ -91,59 +188,58 @@ def mixture_model_setup(
             - location mixture design
             - design_scale matrix
             - scale mixture design
-            - initial mixture weights
             - mixture weight priors (`None` if `pin_mixtures` is not specified`)
     """
+    num_mixtures = mixture_model_desc.shape[1]
+    # num_observations = len(sample_description)
 
     if isinstance(mixture_grouping, str):
         mixture_grouping = sample_description[mixture_grouping]
-    mixture_grouping = mixture_grouping.astype(str)
-
-    groups, init_mixture_assignment = np.unique(mixture_grouping, return_inverse=True)
 
     dmat_loc, design_tensor_loc = _mdesc_setup(
         sample_description=sample_description,
         formula=formula_loc,
-        groups=groups,
+        num_mixtures=num_mixtures,
         differing_factors=differing_factors_loc,
         dim_names=dim_names_loc
     )
     dmat_scale, design_tensor_scale = _mdesc_setup(
         sample_description=sample_description,
         formula=formula_scale,
-        groups=groups,
+        num_mixtures=num_mixtures,
         differing_factors=differing_factors_scale,
         dim_names=dim_names_scale
     )
 
-    num_mixtures = np.size(groups)
-    num_observations = len(sample_description)
     # num_design_params = len(dmat.design_info.column_names)
+    mixture_weight_priors = mixture_prior_setup(
+        mixture_grouping=mixture_grouping,
+        mixture_model_desc=mixture_model_desc
+    )
+    # # build init_mixture_weights matrix
+    # init_mixture_weights = np.zeros(shape=[num_observations, num_mixtures])
+    # init_mixture_weights[np.arange(num_observations), init_mixture_assignment] = 1
+    #
+    # if pin_mixtures is not None:
+    #     pin_mixtures = np.asarray(pin_mixtures)
+    #     if np.issubdtype(pin_mixtures.dtype, np.integer):
+    #         pinned_assignments = pin_mixtures
+    #     else:
+    #         if np.size(pin_mixtures) == 1:
+    #             pin_mixtures = {pin_mixtures.flatten()[0]}
+    #         else:
+    #             pin_mixtures = set(pin_mixtures.astype(str))
+    #         pinned_assignments = np.where(np.vectorize(lambda x: x in pin_mixtures)(mixture_grouping))
+    #
+    #     pin_mask = np.zeros([num_observations, num_mixtures])
+    #     pin_mask[pinned_assignments] = 1
+    #     pin_mask = pin_mask.astype(bool)
+    #
+    #     mixture_weight_priors = np.where(pin_mask, init_mixture_weights, np.ones_like(init_mixture_weights))
+    # else:
+    #     mixture_weight_priors = None
 
-    # build init_mixture_weights matrix
-    init_mixture_weights = np.zeros(shape=[num_observations, num_mixtures])
-    init_mixture_weights[np.arange(num_observations), init_mixture_assignment] = 1
-
-    if pin_mixtures is not None:
-        pin_mixtures = np.asarray(pin_mixtures)
-        if np.issubdtype(pin_mixtures.dtype, np.integer):
-            pinned_assignments = pin_mixtures
-        else:
-            if np.size(pin_mixtures) == 1:
-                pin_mixtures = {pin_mixtures.flatten()[0]}
-            else:
-                pin_mixtures = set(pin_mixtures.astype(str))
-            pinned_assignments = np.where(np.vectorize(lambda x: x in pin_mixtures)(mixture_grouping))
-
-        pin_mask = np.zeros([num_observations, num_mixtures])
-        pin_mask[pinned_assignments] = 1
-        pin_mask = pin_mask.astype(bool)
-
-        mixture_weight_priors = np.where(pin_mask, init_mixture_weights, np.ones_like(init_mixture_weights))
-    else:
-        mixture_weight_priors = None
-
-    return dmat_loc, design_tensor_loc, dmat_scale, design_tensor_scale, init_mixture_weights, mixture_weight_priors
+    return dmat_loc, design_tensor_loc, dmat_scale, design_tensor_scale, mixture_weight_priors
 
 
 def design_tensor_from_mixture_description(

batchglm/train/tf/rsa/base.py
@@ -66,8 +66,8 @@ def param_bounds(dtype):
         "mixture_prob": dtype(1),
         "mixture_log_prob": dtype(0),
         "mixture_logits": dtype(0),
-        "mixture_weight_priors": dtype(1),
-        "mixture_weight_log_priors": dtype(0),
+        "mixture_weight_priors": np.nextafter(dmax, -np.inf, dtype=dtype) / sf,
+        "mixture_weight_log_priors": np.log(np.nextafter(np.inf, -np.inf, dtype=dtype)) / sf,
 
     }
     return bounds_min, bounds_max
@@ -103,7 +103,6 @@ class MixtureModel:
     def __init__(
             self,
             logits,
-            log_priors=None,
             axis=0,
             name="mixture_prob"
     ):
@@ -120,10 +119,6 @@ class MixtureModel:
 
             prob = tf.nn.softmax(logits, axis=axis, name="prob")
             log_prob = tf.nn.log_softmax(logits, axis=axis, name="log_prob")
-
-            if log_priors is not None:
-                log_prob = log_prob + log_priors
-                prob = prob * tf.exp(log_priors)
 
             self.prob = prob
             self.log_prob = log_prob
@@ -146,7 +141,7 @@ class BasicModelGraph:
             mixture_weight_log_priors=None,  # (mixtures, observations)
             size_factors=None,
     ):
-        mixture_model = MixtureModel(logits=mixture_logits, log_priors=mixture_weight_log_priors, axis=-1)
+        mixture_model = MixtureModel(logits=mixture_logits, axis=-1)
         log_mixture_weights = mixture_model.log_prob
         mixture_weights = mixture_model.prob
 
@@ -216,6 +211,10 @@ class BasicModelGraph:
         with tf.name_scope("log_probs"):
             log_probs = dist_obs.log_prob(tf.expand_dims(X, 0), name="log_count_probs")
             log_probs = tf_clip_param(log_probs, "log_probs")
+
+            # add mixture weight priors if specified
+            if mixture_weight_log_priors is not None:
+                log_probs = log_probs + tf.expand_dims(tf.transpose(mixture_weight_log_priors), -1)
 
         # calculate joint probability of mixture distributions
         with tf.name_scope("joint_log_probs"):
@@ -411,16 +410,13 @@ class ModelVars:
             self.mixture_logits_var = mixture_logits
 
             if mixture_weight_priors is not None:
+                mixture_weight_priors = np.asarray(mixture_weight_priors, dtype=dtype.as_numpy_dtype)
                 with tf.name_scope("mixture_weight_priors"):
                     mixture_weight_priors = tf.identity(
                         mixture_weight_priors,
+                        name="mixture_weight_priors"
                     )
-                    mixture_weight_priors = tf.div(
-                        mixture_weight_priors,
-                        tf.reduce_sum(mixture_weight_priors, axis=-1, keepdims=True)
-                    )
-
-                    mixture_weight_priors = tf_clip_param(mixture_weight_priors, "a")
+                    mixture_weight_priors = tf_clip_param(mixture_weight_priors, "mixture_weight_priors")
 
                 mixture_weight_log_priors = tf.log(mixture_weight_priors, name="mixture_weight_log_priors")
             else:

batchglm/train/tf/rsa/estimator.py
@@ -183,10 +183,26 @@ class FullDataModelGraph:
             self.norm_grads_EM_with_grads = norm_grads_EM_with_grads
 
 
-def _normalize_mixture_weights(weights):
-    weights -= np.min(weights, axis=-1, keepdims=True)
-    weights /= np.sum(weights, axis=-1, keepdims=True)
-    weights = np_clip_param(weights, "mixture_prob")
+def _normalize_mixture_weights(weights, shift_to_zero=False):
+    """
+    Normalize mixture weights to (0, 1):
+    $\forall k \in K: w_k = w_k / \sum_x^K w_x$
+    Additionally clips `w` to the range ]0, inf[
+
+    :param weights: the mixture weights with shape (observations, mixtures)
+    :param shift_to_zero: If True, substracts $w = w - min_k(w)$ to make the smallest value to be zero.
+    :return: normalized weights
+    """
+    if shift_to_zero:
+        if isinstance(weights, xr.DataArray):
+            weights -= np.min(weights, axis=-1)
+        else:
+            weights -= np.min(weights, axis=-1, keepdims=True)
+    weights = np_clip_param(weights, "mixture_weight_priors")
+    if isinstance(weights, xr.DataArray):
+        weights /= np.sum(weights, axis=-1)
+    else:
+        weights /= np.sum(weights, axis=-1, keepdims=True)
     return weights
 
 
@@ -219,7 +235,7 @@ class EstimatorGraph(TFEstimatorGraph):
             init_a=None,
             init_b=None,
             init_mixture_weights=None,
-            mixture_weight_log_priors=None,
+            mixture_weight_priors=None,
             summary_mixture_image=False,
             extended_summary=False,
             dtype="float32"
@@ -275,7 +291,7 @@ class EstimatorGraph(TFEstimatorGraph):
                 design_mixture_loc=design_mixture_loc,
                 design_mixture_scale=design_mixture_scale,
                 init_mixture_weights=init_mixture_weights,
-                mixture_weight_priors=mixture_weight_log_priors,
+                mixture_weight_priors=mixture_weight_priors,
                 init_a=init_a,
                 init_b=init_b,
             )
@@ -305,7 +321,7 @@ class EstimatorGraph(TFEstimatorGraph):
                     axis=0,
                     name="batch_mixture_logits"
                 )
-                batch_mixture_weight_priors = tf.gather(
+                batch_mixture_weight_log_priors = tf.gather(
                     model_vars.mixture_weight_log_priors,
                     indices=batch_sample_index,
                     axis=0,
@@ -327,7 +343,7 @@ class EstimatorGraph(TFEstimatorGraph):
                     a=model_vars.a,
                     b=model_vars.b,
                     mixture_logits=batch_mixture_logits,
-                    mixture_weight_log_priors=batch_mixture_weight_priors,
+                    mixture_weight_log_priors=batch_mixture_weight_log_priors,
                     size_factors=batch_size_factors
                 )
                 batch_mixture_EM_update = tf.scatter_update(
@@ -679,6 +695,32 @@ class Estimator(AbstractEstimator, MonitoredTFEstimator, metaclass=abc.ABCMeta):
 
             init_par_link_loc = None
             logger.debug(" * Initialize mean model")
+
+            logger.debug(" * Normalizing weights")
+            init_mixture_weights = _normalize_mixture_weights(init_mixture_weights)
+            # add random uniform error to mixture probabilities with bounds [-random_factor, factor]
+            if random_factor > 0:
+                logger.debug(" * Applying random uniform error of +/- %s", random_factor)
+                init_mixture_weights += np.random.uniform(
+                    low=-random_factor,
+                    high=random_factor,
+                    size=init_mixture_weights.shape
+                )
+                init_mixture_weights = _normalize_mixture_weights(init_mixture_weights)
+
+            if input_data.mixture_weight_priors is not None:
+                # calculate joint weights:
+                # ```
+                #   joint_weights = (
+                #       norm_mixture_weights * norm_priors / sum_mixtures(norm_mixture_weights * norm_priors)
+                #   )
+                # ```
+                normalized_priors = _normalize_mixture_weights(input_data.mixture_weight_priors.astype(float))
+                joint_weights = _normalize_mixture_weights(init_mixture_weights * normalized_priors)
+            else:
+                joint_weights = init_mixture_weights
+            logger.debug("joint weights: %s", joint_weights)
+
             if isinstance(init_a, str):
                 # Chose option if auto was chosen
                 if init_a.lower() == "auto":
@@ -690,7 +732,7 @@ class Estimator(AbstractEstimator, MonitoredTFEstimator, metaclass=abc.ABCMeta):
                         # - concat them to a 3D (mixtures, design_params, features) matrix
                         init_par_link_loc = list()
                         for i in range(input_data.num_mixtures):
-                            weight = init_mixture_weights[:, i]
+                            weight = joint_weights[:, i]
                             groupwise_means, mixture_par_link_loc, rmsd_loc = nb_glm_utils.closedform_nb_glm_logmu(
                                 X=input_data.X,
                                 design_loc=input_data.design_loc,
@@ -757,7 +799,7 @@ class Estimator(AbstractEstimator, MonitoredTFEstimator, metaclass=abc.ABCMeta):
                         # - concat them to a 3D (mixtures, design_params, features) matrix
                         init_par_link_scale = list()
                         for i in range(input_data.num_mixtures):
-                            weight = init_mixture_weights[:, i]
+                            weight = joint_weights[:, i]
                             (
                                 groupwise_scales,
                                 mixture_par_link_scale,
@@ -866,18 +908,6 @@ class Estimator(AbstractEstimator, MonitoredTFEstimator, metaclass=abc.ABCMeta):
             elif init_b is not None:
                 init_b = np.asarray(init_b).astype(dtype)
 
-            logger.debug(" * Normalizing weights")
-            init_mixture_weights = _normalize_mixture_weights(init_mixture_weights)
-            # add random uniform error to mixture probabilities with bounds [-random_factor, factor]
-            if random_factor > 0:
-                logger.debug(" * Applying random uniform error of +/- %s", random_factor)
-                init_mixture_weights += np.random.uniform(
-                    low=-random_factor,
-                    high=random_factor,
-                    size=init_mixture_weights.shape
-                )
-                init_mixture_weights = _normalize_mixture_weights(init_mixture_weights)
-
             logger.debug(" * Start creating model")
             with graph.as_default():
                 # create model
@@ -893,7 +923,7 @@ class Estimator(AbstractEstimator, MonitoredTFEstimator, metaclass=abc.ABCMeta):
                     num_design_mixture_scale_params=input_data.num_design_mixture_scale_params,
                     design_mixture_loc=input_data.design_mixture_loc,
                     design_mixture_scale=input_data.design_mixture_scale,
-                    mixture_weight_log_priors=input_data.mixture_weight_priors,
+                    mixture_weight_priors=input_data.mixture_weight_priors,
                     graph=graph,
                     batch_size=batch_size,
                     init_a=init_a,