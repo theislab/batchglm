49c2052ac4d265cc373d02b13a44718115780f19
ina258 <sabrina.richter100@gmail.com>
2019-03-14 15:25:42 +0100

    added fim

--- batchglm/train/tf/glm_norm/fim.py
+++ batchglm/train/tf/glm_norm/fim.py
@@ -14,7 +14,8 @@ class FIM(FIMGLMALL):
             loc,
             scale
     ):
-        W = tf.square(tf.divide(loc, scale))
+        scalar_one = tf.constant(1, shape=(), dtype=self.dtype)
+        W = tf.square(tf.divide(scalar_one, scale))
 
         return W
 
@@ -24,27 +25,7 @@ class FIM(FIMGLMALL):
             loc,
             scale
     ):
-
-        # TODO
-
-        # scalar_one = tf.constant(1, shape=(), dtype=self.dtype)
-        # scalar_two = tf.constant(2, shape=(), dtype=self.dtype)
-        #
-        # scale_plus_loc = scale + loc
-        # digamma_r = tf.math.digamma(x=scale)
-        # digamma_r_plus_mu = tf.math.digamma(x=scale_plus_loc)
-        #
-        # const1 = tf.multiply(scalar_two, tf.add(
-        #     digamma_r,
-        #     digamma_r_plus_mu
-        # ))
-        # const2 = tf.multiply(scale, tf.add(
-        #     tf.math.polygamma(a=scalar_one, x=scale),
-        #     tf.math.polygamma(a=scalar_one, x=scale_plus_loc)
-        # ))
-        # const3 = tf.divide(scale, scale_plus_loc)
-        # W = tf.multiply(scale, tf.add_n([const1, const2, const3]))
-
-        W = None
+        scalar_two = tf.constant(2, shape=(), dtype=self.dtype)
+        W = scalar_two * tf.ones_like(loc)
 
         return W
