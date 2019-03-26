one small fix and output for (not yet finished) debugging of gradient

batchglm/models/glm_beta/simulator.py
@@ -26,8 +26,8 @@ class Simulator(_Simulator_GLM, Model):
             self,
             rand_fn_ave=lambda shape: np.random.uniform(0.2, 0.8, shape),
             rand_fn=None,
-            rand_fn_loc=lambda shape: np.random.uniform(0.05, 0.15, shape),
-            rand_fn_scale=lambda shape: np.random.uniform(1e5, 2*1e5, shape),
+            rand_fn_loc=lambda shape: np.random.uniform(0.5, 0.6, shape),
+            rand_fn_scale=lambda shape: np.random.uniform(1e1, 2*1e1, shape),
         ):
         self._generate_params(
             self,

batchglm/train/tf/base_glm_all/estimator_graph.py
@@ -532,7 +532,10 @@ class EstimatorGraphAll(EstimatorGraphGLM):
             self.hessians = self.full_data_model.hessians_final
             self.fisher_inv = op_utils.pinv(-self.full_data_model.hessians_final)  # TODO switch for fim?
             # Summary statistics on feature-wise model gradients:
-            self.gradients = tf.reduce_sum(tf.abs(self.full_data_model.neg_jac_final / num_observations), axis=1)
+            a = tf.abs(self.full_data_model.neg_jac_final / num_observations)
+            b = tf.print(a)
+            with tf.control_dependencies([b]):
+                self.gradients = tf.reduce_sum(a, axis=1)
 
         with tf.name_scope('summaries'):
             if extended_summary:

batchglm/train/tf/glm_beta/estimator.py
@@ -116,6 +116,11 @@ class Estimator(EstimatorAll, AbstractEstimator, ProcessModel):
         init_a = init_a.astype(dtype)
         init_b = init_b.astype(dtype)
 
+        print("init_a")
+        print(init_a)
+        print("init_b")
+        print(init_b)
+
         if len(optim_algos) > 0:
             if np.any([x.lower() in ["nr", "nr_tr"] for x in optim_algos]):
                 provide_hessian = True
@@ -210,7 +215,7 @@ class Estimator(EstimatorAll, AbstractEstimator, ProcessModel):
                     init_a = np.zeros([input_data.num_loc_params, input_data.num_features])
                     self._train_loc = True
 
-                    logger.debug("Using all_zero initialization for mean")
+                    logging.getLogger("batchglm").debug("Using all zero initialization for mean")
                 else:
                     raise ValueError("init_a string %s not recognized" % init_a)
                 logging.getLogger("batchglm").debug("Should train mean: %s", self._train_loc)
@@ -224,7 +229,7 @@ class Estimator(EstimatorAll, AbstractEstimator, ProcessModel):
                         design_scale=input_data.design_scale[:, [0]],
                         constraints=input_data.constraints_scale[[0], [0]].values,
                         size_factors=size_factors_init,
-                        groupwise_means=groupwise_means,
+                        groupwise_means=None,
                         link_fn=lambda samplesize: np.log(self.np_clip_param(samplesize, "samplesize"))
                     )
                     init_b = np.zeros([input_data.num_scale_params, input_data.X.shape[1]])

batchglm/train/tf/glm_beta/model.py
@@ -107,8 +107,14 @@ class BasicModelGraph(ProcessModel, BasicModelGraphGLM):
                     - tf.lgamma(one_minus_loc * model_scale)\
                     + (model_scale * model_loc - 1) * tf.log(Xdense)\
                     + (one_minus_loc * model_scale - 1) * tf.log(one_minus_X)
-
-        log_probs = self.tf_clip_param(log_probs, "log_probs")
+        a = tf.print("log_probs: \n", log_probs)
+        b = tf.print("model_loc: \n", model_loc)
+        c = tf.print("model_scale: \n", model_scale)
+        d = tf.print("X: \n", X)
+        e = tf.print("a_var: \n", a_var)
+        f = tf.print("eta_loc: \n", eta_loc)
+        with tf.control_dependencies([a, b, c, d, e, f]):
+            log_probs = self.tf_clip_param(log_probs, "log_probs")
 
         # Variance:
         sigma2 = (model_loc * one_minus_loc) / (1 + model_scale)