e756effa0572fb139cdccec1f5b90f1971163c41
david.seb.fischer <david.seb.fischer@gmail.com>
2019-08-20 11:38:31 +0200

    changed TR to decay smaller, necessary for simulated data to work.

--- batchglm/pkg_constants.py
+++ batchglm/pkg_constants.py
@@ -32,9 +32,9 @@ if TF_NUM_THREADS == 0:
 TRUST_REGION_RADIUS_INIT = 100.
 TRUST_REGION_ETA0 = 0.
 TRUST_REGION_ETA1 = 0.25
-TRUST_REGION_ETA2 = 0.25  # Allow expansion if not shrinking.
-TRUST_REGION_T1 = 0.01  # Fast collapse to avoid trailing.
-TRUST_REGION_T2 = 10.
+TRUST_REGION_ETA2 = 0.25
+TRUST_REGION_T1 = 0.5  # Fast collapse to avoid trailing.
+TRUST_REGION_T2 = 2.  # Allow expansion if not shrinking.
 TRUST_REGION_UPPER_BOUND = 1e5
 
 # Convergence hyper-parameters:
