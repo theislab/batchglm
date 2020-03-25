64186afe31951da2a59ccd8695d1009c588bf34d
picciama <mario.picciani@in.tum.de>
2020-03-25 19:14:22 +0100

    reworked spare featurewise batching method

--- batchglm/train/tf2/base_glm/generator.py
+++ batchglm/train/tf2/base_glm/generator.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.sparse import csr_matrix
 import tensorflow as tf
-
+import time
 
 class DataGenerator:
     """Wrapper Object to generate an iterable TensorFlow Dataset from given input data."""
@@ -26,10 +26,6 @@ class DataGenerator:
         output_types = ((tf.int64, dtp, tf.int64), *(dtp,) * 3) if self.sparse else (dtp,) * 4
         self.dataset = tf.data.Dataset.from_generator(
             generator=self._generate, output_types=output_types)
-        if self.sparse:
-            self.dataset = self.dataset.map(
-                lambda ivs_tuple, loc, scale, sf: (tf.SparseTensor(*ivs_tuple), loc, scale, sf)
-            )
 
     def _generate(self):
         """
@@ -53,10 +49,32 @@ class DataGenerator:
             size_factors = input_data.fetch_size_factors(idx) if fetch_size_factors else 1
             yield counts, dloc, dscale, size_factors
 
+    def _featurewise_batch_sparse(self, ivs_tuple, dloc, dscale, size_factors):
+
+        not_converged = ~self.estimator.model.model_vars.total_converged
+
+        not_converged_numeric = tf.cast(not_converged, dtype=tf.int64)
+        col_idx_map = tf.cumsum(not_converged_numeric, exclusive=True)
+        ivs_tuple[0].set_shape([None, 2])
+        mask = tf.gather(not_converged, ivs_tuple[0][:, 1])
+        new_indices = tf.gather_nd(ivs_tuple[0], tf.where(mask))
+        row_idx, col_idx = tf.split(new_indices, num_or_size_splits=2, axis=1, num=2)
+
+        new_indices = tf.concat([row_idx, tf.gather(col_idx_map, col_idx)], 1)
+        new_values = tf.boolean_mask(ivs_tuple[1], mask)
+        n_features = col_idx_map[-1]
+        if not_converged[-1]:
+            n_features += 1
+
+        x_tensor = (new_indices, new_values, (ivs_tuple[2][0], n_features))
+        return x_tensor, dloc, dscale, size_factors
+
     def _featurewise_batch(self, x_tensor, dloc, dscale, size_factors):
         """Takes an element of a dataset, performs featurewise batching
         and returns the reduced element."""
+
         not_converged = ~self.estimator.model.model_vars.total_converged
+        """
         if self.sparse:
             feature_columns = tf.sparse.split(
                 x_tensor,
@@ -65,14 +83,23 @@ class DataGenerator:
             not_converged_idx = np.where(not_converged)[0]
             feature_columns = [feature_columns[i] for i in not_converged_idx]
             x_tensor = tf.sparse.concat(axis=1, sp_inputs=feature_columns)
-
         else:
-            x_tensor = tf.boolean_mask(tensor=x_tensor, mask=not_converged, axis=1)
+        """
+        x_tensor = tf.boolean_mask(tensor=x_tensor, mask=not_converged, axis=1)
         return x_tensor, dloc, dscale, size_factors
 
     def new_epoch_set(self, batch_features: bool = False):
         """Returns an iterable TensorFlow Dataset of the input data."""
         dataset_to_return = self.dataset.take(self.num_batches)
-        if batch_features:
-            return dataset_to_return.map(self._featurewise_batch).cache().prefetch(1)
-        return self.dataset.take(self.num_batches).cache().prefetch(1)
+
+        if self.sparse:
+            if batch_features:
+                dataset_to_return = dataset_to_return.map(self._featurewise_batch_sparse)
+            dataset_to_return = dataset_to_return.map(
+                lambda ivs_tuple, loc, scale, sf: (tf.SparseTensor(*ivs_tuple), loc, scale, sf)
+            )
+        else:
+            if batch_features:
+                dataset_to_return = dataset_to_return.map(self._featurewise_batch)
+
+        return dataset_to_return.cache().prefetch(1)
