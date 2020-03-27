c2fe15090b35feb5ca7f1479698407ce256271b2
picciama <mario.picciani@in.tum.de>
2020-03-27 23:42:26 +0100

    added boolean for recalc model call if featurewise

--- batchglm/pkg_constants.py
+++ batchglm/pkg_constants.py
@@ -32,7 +32,8 @@ GTOL_BY_FEATURE_SCALE = 1e-8
 TRTOL_BY_FEATURE_LOC = 1e-12
 TRTOL_BY_FEATURE_SCALE = 1e-12
 
-FEATUREWISE_THRESHOLD = 100  # the minimal number of features to converge before next featurewise batch
+FEATUREWISE_THRESHOLD = 1  # the minimal number of features to converge before next featurewise batch
+FEATUREWISE_RECALCULATE = False # if set to True, recalculate the results from the previous train step
 
 try:
     import tensorflow as tf
