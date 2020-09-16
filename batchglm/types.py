6227b77c75bf739b1a65e48ef927ebc973050f7d
michalk8 <michal.klein@protonmail.com>
2020-09-16 20:22:43 +0200

    Fix missing import, remove TypeAlias

--- batchglm/types.py
+++ batchglm/types.py
@@ -1,12 +1,13 @@
-from typing import TypeAlias, TypeVar, Union
+from typing import TypeVar, Union
 
 import scipy
 import dask
+import numpy as np
 
 try:
     from anndata import AnnData
 except ImportError:
     AnnData = TypeVar("AnnData")
 
-ArrayLike = TypeAlias(Union[np.ndarray, scipy.sparse.csr_matrix, dask.array.core.Array])
-InputType = TypeAlias(Union[ArrayLike, AnnData, "InputDataBase"])
+ArrayLike = Union[np.ndarray, scipy.sparse.csr_matrix, dask.array.core.Array]
+InputType = Union[ArrayLike, AnnData, "InputDataBase"]
