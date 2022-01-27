12e75cd84ab5063ddffa866c46b5078e11d7b7a4
ilan-gold <ilanbassgold@gmail.com>
2022-01-27 18:56:32 +0100

    Remove constants.

--- batchglm/pkg_constants.py
+++ batchglm/pkg_constants.py
@@ -26,23 +26,3 @@ XTOL_BY_FEATURE_LOC = 1e-8
 XTOL_BY_FEATURE_SCALE = 1e-6
 GTOL_BY_FEATURE_LOC = 1e-8
 GTOL_BY_FEATURE_SCALE = 1e-8
-
-try:
-
-    TF_NUM_THREADS = int(os.environ.get('TF_NUM_THREADS', 0))
-    TF_LOOP_PARALLEL_ITERATIONS = int(os.environ.get('TF_LOOP_PARALLEL_ITERATIONS', 10))
-
-    TF_CONFIG_PROTO = tf.compat.v1.ConfigProto()
-    TF_CONFIG_PROTO.allow_soft_placement = True
-    TF_CONFIG_PROTO.log_device_placement = False
-    TF_CONFIG_PROTO.gpu_options.allow_growth = True
-    TF_CONFIG_PROTO.graph_options.optimizer_options.global_jit_level = tf.compat.v1.OptimizerOptions.ON_1
-
-    TF_CONFIG_PROTO.inter_op_parallelism_threads = TF_NUM_THREADS
-    TF_CONFIG_PROTO.intra_op_parallelism_threads = TF_NUM_THREADS
-
-    if TF_NUM_THREADS == 0:
-        TF_NUM_THREADS = multiprocessing.cpu_count()
-
-except ImportError:
-    tf = None
