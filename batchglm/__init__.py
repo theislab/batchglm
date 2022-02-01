52abdd5cd4637b1e2a6705d62651ae5312f2cf18
picciama <mario.picciani@in.tum.de>
2022-02-01 17:29:36 +0100

    added SPARSE_AUTO_DENSIFY for sparse arrays:
    This solves an issue between dask and sparse where sparse matrices are not automatically converted to dense.
    Setting the environment variable will activate auto conversion.

--- batchglm/__init__.py
+++ batchglm/__init__.py
@@ -4,3 +4,7 @@ __version__ = get_versions()['version']
 del get_versions
 
 from .log_cfg import logger, unconfigure_logging, setup_logging
+
+import os
+# we need this for the sparse package, see https://github.com/pydata/sparse/issues/10
+os.environ["SPARSE_AUTO_DENSIFY"] = "1"
