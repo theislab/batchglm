eb41cc0832b680d73b61c382d4692aa78edae505
picciama <mario.picciani@in.tum.de>
2022-02-03 16:18:07 +0100

    formatted by black

--- batchglm/train/numpy/glm_nb/external.py
+++ batchglm/train/numpy/glm_nb/external.py
@@ -4,6 +4,7 @@ from batchglm.models.base_glm import InputDataGLM
 from batchglm.models.base_glm.utils import closedform_glm_mean, closedform_glm_scale
 from batchglm.models.glm_nb import Model, _EstimatorGLM
 from batchglm.models.glm_nb.utils import init_par
+
 # import necessary base_glm layers
 from batchglm.train.numpy.base_glm import EstimatorGlm, ModelIwls, ModelVarsGlm, ProcessModelGlm
 from batchglm.utils.linalg import groupwise_solve_lm
