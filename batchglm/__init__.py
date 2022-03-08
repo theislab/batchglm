eb293c85e531a3e2c7c2412e6075b113655eeee2
picciama <mario.picciani@in.tum.de>
2022-03-09 00:37:39 +0100

    removed _version for now

--- batchglm/__init__.py
+++ batchglm/__init__.py
@@ -1,11 +1,12 @@
 import os
 
 from . import models, pkg_constants, train, utils
-from ._version import get_versions
+
+# from ._version import get_versions
 from .log_cfg import logger, setup_logging, unconfigure_logging
 
-__version__ = get_versions()["version"]
-del get_versions
+# __version__ = _version.get_versions()["version"]
+# del get_versions
 
 # we need this for the sparse package, see https://github.com/pydata/sparse/issues/10
 os.environ["SPARSE_AUTO_DENSIFY"] = "1"
