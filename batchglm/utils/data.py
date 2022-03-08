3e337d73a8957be1c85fb96d28287e6add861324
picciama <mario.picciani@in.tum.de>
2022-03-08 20:01:02 +0100

    cleanup and fix mypy issues

--- batchglm/utils/data.py
+++ batchglm/utils/data.py
@@ -1,5 +1,6 @@
 import logging
-from typing import List, Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union, overload, Dict
+
 from functools import singledispatch
 
 import numpy as np
@@ -130,14 +131,20 @@ def preview_coef_names(
     return coef_names
 
 
+def _assert_design_mat_full_rank(cmat, dmat):
+    # Test full design matrix for being full rank before returning:
+    if cmat is None:
+        assert np.linalg.matrix_rank(dmat) == dmat.shape[1], "constrained design matrix is not full rank: %i %i" % (np.linalg.matrix_rank(dmat), dmat.shape[1])
+    else:
+        assert np.linalg.matrix_rank(np.matmul(dmat, cmat)) == cmat.shape[1],  "constrained design matrix is not full rank: %i %i" % (np.linalg.matrix_rank(np.matmul(dmat, cmat)), cmat.shape[1])
+
+
 @singledispatch
 def constraint_system_from_star(
-    constraints,
+    constraints: Optional[np.ndarray] = None,
     dmat: Optional[Union[patsy.design_info.DesignMatrix, pd.DataFrame]] = None,
-    sample_description: Optional[pd.DataFrame] = None,
-    formula: Optional[str] = None,
-    as_categorical: Union[bool, list] = True,
-    return_type: str = "patsy",
+    term_names: Optional[List[str]] = None,
+    **kwargs
 ) -> Tuple:
     """
     Wrap different constraint matrix building formats with building of design matrix.
@@ -198,14 +205,19 @@ def constraint_system_from_star(
     """
     if constraints is None:
         cmat = None
-        term_names = None
-        if isinstance(dmat, pd.DataFrame):
-            coef_names = dmat.columns
-            dmat = dmat.values
-        else:
-            coef_names = dmat.design_info.column_names
+    elif isinstance(constraints, np.ndarray):
+        cmat = constraints
     else:
-        raise ValueError("constraint format %s not recognized" % type(constraints))
+        raise TypeError(f"Type {type(constraints)} not recognized for argument constraints.")
+    if dmat is None:
+        dmat, coef_names = design_matrix(**kwargs)
+    if isinstance(dmat, pd.DataFrame):
+        coef_names = dmat.columns.to_list()
+        dmat = dmat.values
+    elif isinstance(dmat, patsy.DesignMatrix):
+        coef_names = dmat.design_info.column_names
+    else:
+        raise TypeError(f"Type {type(dmat)} not recognized for argument dmat.")
 
     # Test full design matrix for being full rank before returning:
     if cmat is None:
@@ -219,107 +231,94 @@ def constraint_system_from_star(
                 "constrained design matrix is not full rank: %i %i"
                 % (np.linalg.matrix_rank(np.matmul(dmat, cmat)), cmat.shape[1])
             )
-
     return dmat, coef_names, cmat, term_names
 
-def _assert_design_mat_full_rank(cmat, dmat):
-    # Test full design matrix for being full rank before returning:
-    if cmat is None:
-        assert np.linalg.matrix_rank(dmat) == dmat.shape[1], "constrained design matrix is not full rank: %i %i" % (np.linalg.matrix_rank(dmat), dmat.shape[1])
-    else:
-        assert np.linalg.matrix_rank(np.matmul(dmat, cmat)) == cmat.shape[1],  "constrained design matrix is not full rank: %i %i" % (np.linalg.matrix_rank(np.matmul(dmat, cmat)), cmat.shape[1])
 
 @constraint_system_from_star.register
-def _(
+def _constraint_system_from_dict(
     constraints: dict,
-    dmat: Optional[Union[patsy.design_info.DesignMatrix, pd.DataFrame]] = None,
-    sample_description: Optional[pd.DataFrame] = None,
-    formula: Optional[str] = None,
-    as_categorical: Union[bool, list] = True,
     return_type: str = "patsy",
-):
-    if sample_description is None and dmat is None:
-        raise ValueError("supply either sample_description or dmat")
-    if dmat is not None and isinstance(constraints, dict):
-        raise ValueError("dmat was supplied even though constraints were given as dict")
-    if formula is None:
-        raise ValueError("Provide a formula when providing constraints as dict.")
-    dmat, coef_names, cmat, term_names = constraint_matrix_from_dict(
-        sample_description=sample_description,
-        formula=formula,
-        as_categorical=as_categorical,
-        constraints=constraints,
-        return_type="patsy",
-    )
-    _assert_design_mat_full_rank(cmat, dmat)
-
-    return dmat, coef_names, cmat, term_names
+    **kwargs,
+) -> Tuple:
+    cmat, dmat, term_names = constraint_system_from_dict(constraints, **kwargs)
+    return constraint_system_from_star(cmat, dmat=dmat, return_type=return_type, term_names=term_names, **kwargs)
 
 @constraint_system_from_star.register
-def _(
-    constraints: list, # something wrong with using tuples here
-    dmat: Optional[Union[patsy.design_info.DesignMatrix, pd.DataFrame]] = None,
-    sample_description: Optional[pd.DataFrame] = None,
-    formula: Optional[str] = None,
-    as_categorical: Union[bool, list] = True,
-    return_type: str = "patsy",
-):
-    if sample_description is None and dmat is None:
-        raise ValueError("supply either sample_description or dmat")
+def _constraint_system_from_list(
+    constraints: list,
+    dmat: Optional[Union[patsy.DesignMatrix, pd.DataFrame]] = None,
+    return_type: str = 'patsy',
+    **kwargs,
+) -> Tuple:
     if dmat is None:
-        dmat, coef_names = design_matrix(
-            sample_description=sample_description,
-            formula=formula,
-            as_categorical=as_categorical,
-            dmat=None,
-            return_type=return_type,
-        )
-    cmat = constraint_matrix_from_string(
-        dmat=dmat, coef_names=dmat.design_info.column_names, constraints=constraints
-    )
-    term_names = None  # not supported yet.
-    _assert_design_mat_full_rank(cmat, dmat)
-
-    return dmat, coef_names, cmat, term_names
-
-@constraint_system_from_star.register
-def _(
-    constraints: np.ndarray,
-    dmat: Optional[Union[patsy.design_info.DesignMatrix, pd.DataFrame]] = None,
-    sample_description: Optional[pd.DataFrame] = None,
-    formula: Optional[str] = None,
-    as_categorical: Union[bool, list] = True,
-    return_type: str = "patsy",
-):
-    if sample_description is None and dmat is None:
-        raise ValueError("supply either sample_description or dmat")
-    cmat = constraints
-    term_names = None
-    if dmat is None:
-        dmat, coef_names = design_matrix(
-            sample_description=sample_description,
-            formula=formula,
-            as_categorical=as_categorical,
-            dmat=None,
-            return_type=return_type,
-        )
+        dmat, _ = design_matrix(**kwargs)
     if isinstance(dmat, pd.DataFrame):
-        coef_names = dmat.columns
-        dmat = dmat.values
+        cmat = constraint_matrix_from_string(
+            dmat=dmat.to_numpy(), coef_names=dmat.columns.to_numpy(), constraints=constraints
+        )
+    elif isinstance(dmat, patsy.DesignMatrix):
+        cmat = constraint_matrix_from_string(
+            dmat=dmat, coef_names=dmat.design_info.column_names, constraints=constraints
+        )
     else:
-        coef_names = dmat.design_info.column_names
-    _assert_design_mat_full_rank(cmat, dmat)
+        raise TypeError(f'Type {type(dmat)} not recognized for argument dmat.')
 
-    return dmat, coef_names, cmat, term_names
+    return constraint_system_from_star(cmat, dmat=dmat, return_type=return_type, **kwargs)
 
 
-def constraint_matrix_from_dict(
+def string_constraints_from_dict(sample_description: pd.DataFrame, constraints: Optional[dict] = None):
+    r"""
+    Create string-encoded constraints from dictionary encoded constraints and sample description.
+
+    :param sample_description: pandas.DataFrame of length "num_observations" containing explanatory variables as columns
+    :param constraints: Grouped factors to enfore equality constraints on. Every element of
+        the dictionary corresponds to one set of equality constraints. Each set has to be
+        be an entry of the form {..., x: y, ...} where x is the factor to be constrained and y is
+        a factor by which levels of x are grouped and then constrained. Set y="1" to constrain
+        all levels of x to sum to one, a single equality constraint.
+
+            E.g.: {"batch": "condition"} Batch levels within each condition are constrained to sum to
+                zero. This is applicable if repeats of a an experiment within each condition
+                are independent so that the set-up ~1+condition+batch is perfectly confounded.
+
+        Can only group by non-constrained effects right now, use constraint_matrix_from_string
+        for other cases.
+    :return: List of constraints as strings.
+
+        E.g. ["batch1 + batch2 + batch3 = 0"]
+    """
+    constraints_ls = []
+    if constraints is not None:
+        for key, value in constraints.items():
+            assert isinstance(key, str), "constrained should contain strings"
+            dmat_constrained_temp = patsy.highlevel.dmatrix("0+" + key, sample_description)
+
+            dmat_grouping_temp = patsy.highlevel.dmatrix("0+" + value, sample_description)
+            for j in range(dmat_grouping_temp.shape[1]):
+                grouping = dmat_grouping_temp[:, j]
+                idx_constrained_group = np.where(np.sum(dmat_constrained_temp[grouping == 1, :], axis=0) > 0)[0]
+                # Assert that required grouping is nested.
+                assert np.all(
+                    np.logical_xor(
+                        np.sum(dmat_constrained_temp[grouping == 1, :], axis=0) > 0,
+                        np.sum(dmat_constrained_temp[grouping == 0, :], axis=0) > 0,
+                    )
+                ), "proposed grouping of constraints is not nested, read docstrings"
+                # Add new string-encoded equality constraint.
+                constraints_ls.append(
+                    "+".join(list(np.asarray(dmat_constrained_temp.design_info.column_names)[idx_constrained_group]))
+                    + "=0"
+                )
+
+        logging.getLogger("batchglm").warning("Built constraints: " + ", ".join(constraints_ls))
+    return constraints_ls
+
+def constraint_system_from_dict(
+    constraints: dict,
     sample_description: pd.DataFrame,
     formula: str,
-    as_categorical: Union[bool, list] = True,
-    constraints: Optional[dict] = None,
-    return_type: str = "patsy",
-) -> Tuple:
+    as_categorical: Union[bool, List[str], pd.Index, np.ndarray] = True,
+) -> Tuple[np.ndarray, patsy.DesignMatrix, List[str]]:
     """
     Create a design matrix from some sample description and a constraint matrix
     based on factor encoding of constrained parameter sets.
@@ -358,25 +357,21 @@ def constraint_matrix_from_dict(
         - model design matrix
         - term_names to allow slicing by factor if return type cannot be patsy.DesignMatrix
     """
-    if constraints is None:
-        constraints = {}
-    assert len(constraints) > 0, "supply constraints"
-    sample_description: pd.DataFrame = sample_description.copy()
-
-    if type(as_categorical) is not bool or as_categorical:
-        if type(as_categorical) is bool and as_categorical:
-            as_categorical = np.repeat(True, sample_description.columns.size)
-
-        for to_cat, col in zip(as_categorical, sample_description):
-            if to_cat:
-                sample_description[col] = sample_description[col].astype("category")
+    # input sanity checks
+    assert len(constraints) > 0, "Constraints must not be empty."
+    sample_description = sample_description.copy()
+    if isinstance(as_categorical, bool) and as_categorical:
+        as_categorical = sample_description.columns
+    
+    #make columns categorical
+    sample_description[as_categorical] = sample_description[as_categorical].astype('category')
 
     # Build core design matrix on unconstrained factors. Then add design matrices without
     # absorption of the first level of each factor for each constrained factor onto the
     # core matrix.
-    formula_unconstrained = formula.split("+")
-    formula_unconstrained = [x for x in formula_unconstrained if x.strip(" ") not in constraints.keys()]
-    formula_unconstrained = "+".join(formula_unconstrained)
+    formula_unconstrained_list = formula.split("+")
+    formula_unconstrained_list = [x for x in formula_unconstrained_list if x.strip(" ") not in constraints.keys()]
+    formula_unconstrained = "+".join(formula_unconstrained_list)
     dmat = patsy.dmatrix(formula_unconstrained, sample_description)
     coef_names = dmat.design_info.column_names
     term_names = dmat.design_info.term_names
@@ -390,66 +385,12 @@ def constraint_matrix_from_dict(
         term_names.extend(dmat_constrained_temp.design_info.term_names)
 
     # Build constraint matrix.
-    constraints_ar = constraint_matrix_from_string(dmat=dmat, coef_names=coef_names, constraints=constraints_ls)
+    cmat = constraint_matrix_from_string(dmat=dmat, coef_names=coef_names, constraints=constraints_ls)
+    dmat = pd.DataFrame(dmat, columns=coef_names)
 
-    # Format return type
-    if return_type == "dataframe":
-        dmat = pd.DataFrame(dmat, columns=coef_names)
-    return dmat, coef_names, constraints_ar, term_names
+    return cmat, dmat, term_names
 
-
-def string_constraints_from_dict(sample_description: pd.DataFrame, constraints: Optional[dict] = None):
-    r"""
-    Create string-encoded constraints from dictionary encoded constraints and sample description.
-
-    :param sample_description: pandas.DataFrame of length "num_observations" containing explanatory variables as columns
-    :param constraints: Grouped factors to enfore equality constraints on. Every element of
-        the dictionary corresponds to one set of equality constraints. Each set has to be
-        be an entry of the form {..., x: y, ...} where x is the factor to be constrained and y is
-        a factor by which levels of x are grouped and then constrained. Set y="1" to constrain
-        all levels of x to sum to one, a single equality constraint.
-
-            E.g.: {"batch": "condition"} Batch levels within each condition are constrained to sum to
-                zero. This is applicable if repeats of a an experiment within each condition
-                are independent so that the set-up ~1+condition+batch is perfectly confounded.
-
-        Can only group by non-constrained effects right now, use constraint_matrix_from_string
-        for other cases.
-    :return: List of constraints as strings.
-
-        E.g. ["batch1 + batch2 + batch3 = 0"]
-    """
-    if constraints is not None:
-        constraints_ls = []
-        for i, x in enumerate(constraints.keys()):
-            assert isinstance(x, str), "constrained should contain strings"
-            dmat_constrained_temp = patsy.highlevel.dmatrix("0+" + x, sample_description)
-
-            dmat_grouping_temp = patsy.highlevel.dmatrix("0+" + list(constraints.values())[i], sample_description)
-            for j in range(dmat_grouping_temp.shape[1]):
-                grouping = dmat_grouping_temp[:, j]
-                idx_constrained_group = np.where(np.sum(dmat_constrained_temp[grouping == 1, :], axis=0) > 0)[0]
-                # Assert that required grouping is nested.
-                assert np.all(
-                    np.logical_xor(
-                        np.sum(dmat_constrained_temp[grouping == 1, :], axis=0) > 0,
-                        np.sum(dmat_constrained_temp[grouping == 0, :], axis=0) > 0,
-                    )
-                ), "proposed grouping of constraints is not nested, read docstrings"
-                # Add new string-encoded equality constraint.
-                constraints_ls.append(
-                    "+".join(list(np.asarray(dmat_constrained_temp.design_info.column_names)[idx_constrained_group]))
-                    + "=0"
-                )
-
-        logging.getLogger("batchglm").warning("Built constraints: " + ", ".join(constraints_ls))
-    else:
-        constraints_ls = None
-
-    return constraints_ls
-
-
-def constraint_matrix_from_string(dmat: np.ndarray, coef_names: list, constraints: Union[Tuple[str, str], List[str]]):
+def constraint_matrix_from_string(dmat: np.ndarray, coef_names: list, constraints: List[str]) -> np.ndarray:
     r"""
     Create constraint matrix form string encoded equality constraints.
 
@@ -497,7 +438,7 @@ def constraint_matrix_from_string(dmat: np.ndarray, coef_names: list, constraint
 
 
 def bin_continuous_covariate(
-    sample_description: pd.DataFrame, factor_to_bin: str, bins: Union[int, list, np.ndarray, Tuple]
+    sample_description: pd.DataFrame, factor_to_bin: str, bins: Union[int, List[Union[int, float]], np.ndarray, Tuple]
 ):
     r"""
     Bin a continuous covariate.
@@ -510,11 +451,12 @@ def bin_continuous_covariate(
         quantiles of the covariate, ie the bottom 20% of observations are in the first bin if bins==5.
     :return: Sample description table with binned covariate added.
     """
-    if isinstance(bins, list) or isinstance(bins, np.ndarray) or isinstance(bins, Tuple):
+    if isinstance(bins, int):
+        bins = np.arange(0, 1, 1 / bins)
+    if isinstance(bins, (list, np.ndarray, tuple)):
         bins = np.asarray(bins)
     else:
-        bins = np.arange(0, 1, 1 / bins)
-
+        raise TypeError(f"Type {type(bins)} recognized for argument bins.")
     sample_description[factor_to_bin + "_binned"] = np.digitize(
         np.argsort(np.argsort(sample_description[factor_to_bin].values)) / sample_description.shape[0], bins
     )
