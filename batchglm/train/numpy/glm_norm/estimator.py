b48f54b5d2ea71632191a27241bcdca74407c7bb
ilan-gold <ilanbassgold@gmail.com>
2022-03-16 09:23:45 -0400

    [WIP] Refactor to fit model, accuracy test passes.

--- batchglm/train/numpy/glm_norm/estimator.py
+++ batchglm/train/numpy/glm_norm/estimator.py
@@ -14,7 +14,7 @@ import scipy.optimize
 import scipy.sparse
 import sparse
 
-from .external import pkg_constants, EstimatorGlm, Model, init_par
+from .external import pkg_constants, EstimatorGlm, Model, closedform_norm_glm_logsd, closedform_norm_glm_mean
 from .model_container import ModelContainer
 
 logger = logging.getLogger("batchglm")
@@ -39,10 +39,9 @@ class Estimator(EstimatorGlm):
         :param dtype:
             i.e float64
         """
-        init_theta_location, init_theta_scale, _, _ = init_par(
-            model=model, init_location=init_location, init_scale=init_scale
-        )
+        init_theta_location = np.zeros([model.num_loc_params, model.num_features])
         init_theta_location = init_theta_location.astype(dtype)
+        init_theta_scale = np.zeros([model.num_scale_params, model.x.shape[1]])
         init_theta_scale = init_theta_scale.astype(dtype)
         _model_container = ModelContainer(
             model=model,
@@ -57,4 +56,18 @@ class Estimator(EstimatorGlm):
         self,
         **kwargs,
     ):
-        pass
+        model = self._model_container.model
+        _, theta_location, _ = closedform_norm_glm_mean(
+            x=model.x,
+            design_loc=model.design_loc,
+            constraints_loc=model.constraints_loc,
+            size_factors=model.size_factors,
+        )
+        _, theta_scale, _ = closedform_norm_glm_logsd(
+            x=model.x,
+            design_scale=model.design_scale,
+            constraints=model.constraints_scale,
+            size_factors=model.size_factors,
+        )
+        self._model_container.theta_location = theta_location
+        self._model_container.theta_scale = theta_scale
--- batchglm/train/numpy/glm_norm/external.py
+++ batchglm/train/numpy/glm_norm/external.py
@@ -2,7 +2,7 @@ import batchglm.utils.data as data_utils
 from batchglm import pkg_constants
 from batchglm.models.base_glm.utils import closedform_glm_mean, closedform_glm_scale
 from batchglm.models.glm_norm.model import Model
-from batchglm.models.glm_norm.utils import init_par
+from batchglm.models.glm_norm.utils import closedform_norm_glm_logsd, closedform_norm_glm_mean
 
 # import necessary base_glm layers
 from batchglm.train.numpy.base_glm import BaseModelContainer, EstimatorGlm
