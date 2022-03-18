d397229c295f26d75b95d24d6229e9946d482c83
ilan-gold <ilanbassgold@gmail.com>
2022-03-17 22:02:57 -0400

    [WIP] Need to log likelihood

---
+++ batchglm/train/numpy/glm_poisson/estimator.py
@@ -0,0 +1,77 @@
+import sys
+from typing import Optional, Tuple, Union
+
+import numpy as np
+
+from .external import EstimatorGlm, Model, init_par
+from .model_container import ModelContainer
+
+
+class Estimator(EstimatorGlm):
+    """
+    Estimator for Generalized Linear Models (GLMs) with negative binomial noise.
+    Uses the natural logarithm as linker function.
+
+    Attributes
+    ----------
+    model_vars : ModelVars
+        model variables
+    """
+
+    def __init__(
+        self,
+        model: Model,
+        init_location: str = "AUTO",
+        init_scale: str = "AUTO",
+        # batch_size: Optional[Union[Tuple[int, int], int]] = None,
+        quick_scale: bool = False,
+        dtype: str = "float64",
+    ):
+        """
+        Performs initialisation and creates a new estimator.
+
+        :param init_location: (Optional)
+            Low-level initial values for a. Can be:
+
+            - str:
+                * "auto": automatically choose best initialization
+                * "random": initialize with random values
+                * "standard": initialize intercept with observed mean
+                * "init_model": initialize with another model (see `ìnit_model` parameter)
+                * "closed_form": try to initialize with closed form
+            - np.ndarray: direct initialization of 'a'
+        :param init_scale: (Optional)
+            Low-level initial values for b. Can be:
+
+            - str:
+                * "auto": automatically choose best initialization
+                * "random": initialize with random values
+                * "standard": initialize with zeros
+                * "init_model": initialize with another model (see `ìnit_model` parameter)
+                * "closed_form": try to initialize with closed form
+            - np.ndarray: direct initialization of 'b'
+        :param quick_scale: bool
+            Whether `scale` will be fitted faster and maybe less accurate.
+            Useful in scenarios where fitting the exact `scale` is not absolutely necessary.
+        :param dtype: Numerical precision.
+        """
+        init_theta_location, init_theta_scale, train_loc, train_scale = init_par(
+            model=model, init_location=init_location, init_scale=init_scale
+        )
+        self._train_loc = train_loc
+        self._train_scale = train_scale
+        if quick_scale:
+            self._train_scale = False
+        sys.stdout.write("training location model: %s\n" % str(self._train_loc))
+        sys.stdout.write("training scale model: %s\n" % str(self._train_scale))
+        init_theta_location = init_theta_location.astype(dtype)
+        init_theta_scale = init_theta_scale.astype(dtype)
+
+        _model_container = ModelContainer(
+            model=model,
+            init_theta_location=init_theta_location,
+            init_theta_scale=init_theta_scale,
+            chunk_size_genes=model.chunk_size_genes,
+            dtype=dtype,
+        )
+        super(Estimator, self).__init__(model_container=_model_container, dtype=dtype)
---
+++ batchglm/train/numpy/glm_poisson/model_container.py
@@ -0,0 +1,181 @@
+from typing import Callable, Union
+
+import dask
+import numpy as np
+import scipy
+
+from .external import BaseModelContainer
+
+
+class ModelContainer(BaseModelContainer):
+    @property
+    def fim_weight(self):
+        raise NotImplementedError("This method is currently unimplemented as it isn't used by any built-in procedures.")
+
+    @property
+    def fim_weight_location_location(self) -> Union[np.ndarray, dask.array.core.Array]:
+        """
+        Fisher inverse matrix weights
+        :return: observations x features
+        """
+        return self.location
+
+    @property
+    def ybar(self) -> Union[np.ndarray, dask.array.core.Array]:
+        """
+        :return: observations x features
+        """
+        return np.asarray(self.x - self.location) / self.location
+
+    def fim_weight_location_location_j(self, j) -> Union[np.ndarray, dask.array.core.Array]:
+        """
+        Fisher inverse matrix weights at j
+        :return: observations x features
+        """
+        return self.location_j(j=j)
+
+    def ybar_j(self, j) -> Union[np.ndarray, dask.array.core.Array]:
+        """
+        :return: observations x features
+        """
+        # Make sure that dimensionality of sliced array is kept:
+        if isinstance(j, int) or isinstance(j, np.int32) or isinstance(j, np.int64):
+            j = [j]
+        if isinstance(self.x, np.ndarray) or isinstance(self.x, dask.array.core.Array):
+            return (self.x[:, j] - self.location_j(j=j)) / self.location_j(j=j)
+        else:
+            return np.asarray(self.x[:, j] - self.location_j(j=j)) / self.location_j(j=j)
+
+    @property
+    def jac_weight(self):
+        raise NotImplementedError("This method is currently unimplemented as it isn't used by any built-in procedures.")
+
+    @property
+    def jac_weight_j(self):
+        raise NotImplementedError("This method is currently unimplemented as it isn't used by any built-in procedures.")
+
+    @property
+    def jac_weight_scale(self) -> Union[np.ndarray, dask.array.core.Array]:
+        pass
+
+    def jac_weight_scale_j(self, j) -> Union[np.ndarray, dask.array.core.Array]:
+        pass
+
+    @property
+    def fim_location_scale(self) -> np.ndarray:
+        pass
+
+    @property
+    def fim_scale_scale(self) -> np.ndarray:
+        pass
+
+    @property
+    def hessian_weight_location_scale(self) -> np.ndarray:
+        pass
+
+    @property
+    def hessian_weight_location_location(self) -> np.ndarray:
+        """location-location block of the hessian matrix"""
+        return self.location
+
+    @property
+    def hessian_weight_scale_scale(self) -> np.ndarray:
+        pass
+
+    @property
+    def ll(self) -> Union[np.ndarray, dask.array.core.Array]:
+        """log-likelihood"""
+        scale = self.scale
+        loc = self.location
+        log_r_plus_mu = np.log(scale + loc)
+        if isinstance(self.x, np.ndarray) or isinstance(self.x, dask.array.core.Array):
+            # dense numpy or dask
+            ll = (
+                scipy.special.gammaln(scale + self.x)
+                - scipy.special.gammaln(self.x + np.ones_like(scale))
+                - scipy.special.gammaln(scale)
+                + self.x * (self.eta_loc - log_r_plus_mu)
+                + np.multiply(scale, self.eta_scale - log_r_plus_mu)
+            )
+        else:
+            # sparse scipy
+            ll = (
+                scipy.special.gammaln(np.asarray(scale + self.x))
+                - scipy.special.gammaln(self.x + np.ones_like(scale))
+                - scipy.special.gammaln(scale)
+                + np.asarray(
+                    self.x.multiply(self.eta_loc - log_r_plus_mu) + np.multiply(scale, self.eta_scale - log_r_plus_mu)
+                )
+            )
+            ll = np.asarray(ll)
+        return self.np_clip_param(ll, "ll")
+
+    def ll_j(self, j) -> Union[np.ndarray, dask.array.core.Array]:
+        """
+        Log likelhiood for observation j
+        :param j: observation
+        """
+        # Make sure that dimensionality of sliced array is kept:
+        if isinstance(j, int) or isinstance(j, np.int32) or isinstance(j, np.int64):
+            j = [j]
+        scale = self.scale_j(j=j)
+        loc = self.location_j(j=j)
+        log_r_plus_mu = np.log(scale + loc)
+        if isinstance(self.x, np.ndarray) or isinstance(self.x, dask.array.core.Array):
+            # dense numpy or dask
+            ll = (
+                scipy.special.gammaln(scale + self.x[:, j])
+                - scipy.special.gammaln(self.x[:, j] + np.ones_like(scale))
+                - scipy.special.gammaln(scale)
+                + self.x[:, j] * (self.eta_loc_j(j=j) - log_r_plus_mu)
+                + np.multiply(scale, self.eta_scale_j(j=j) - log_r_plus_mu)
+            )
+        else:
+            # sparse scipy
+            ll = (
+                scipy.special.gammaln(np.asarray(scale + self.x[:, j]))
+                - scipy.special.gammaln(self.x + np.ones_like(scale))
+                - scipy.special.gammaln(scale)
+                + np.asarray(
+                    self.x[:, j].multiply(self.eta_loc_j(j=j) - log_r_plus_mu)
+                    + np.multiply(scale, self.eta_scale_j(j=j) - log_r_plus_mu)
+                )
+            )
+            ll = np.asarray(ll)
+        return self.np_clip_param(ll, "ll")
+
+    def ll_handle(self) -> Callable:
+        def fun(x, eta_loc, theta_scale, xh_scale):
+            eta_scale = np.matmul(xh_scale, theta_scale)
+            scale = np.exp(eta_scale)
+            loc = np.exp(eta_loc)
+            log_r_plus_mu = np.log(scale + loc)
+            if isinstance(x, np.ndarray) or isinstance(x, dask.array.core.Array):
+                # dense numpy or dask
+                ll = (
+                    scipy.special.gammaln(scale + x)
+                    - scipy.special.gammaln(x + np.ones_like(scale))
+                    - scipy.special.gammaln(scale)
+                    + x * (eta_loc - log_r_plus_mu)
+                    + np.multiply(scale, eta_scale - log_r_plus_mu)
+                )
+            else:
+                raise ValueError("type x %s not supported" % type(x))
+            return self.np_clip_param(ll, "ll")
+
+        return fun
+
+    def jac_scale_handle(self) -> Callable:
+        def fun(x, eta_loc, theta_scale, xh_scale):
+            scale = np.exp(theta_scale)
+            loc = np.exp(eta_loc)
+            scale_plus_x = scale + x
+            r_plus_mu = scale + loc
+
+            # Define graphs for individual terms of constant term of hessian:
+            const1 = scipy.special.digamma(scale_plus_x) - scipy.special.digamma(scale)
+            const2 = -scale_plus_x / r_plus_mu
+            const3 = np.log(scale) + np.ones_like(scale) - np.log(r_plus_mu)
+            return scale * (const1 + const2 + const3)
+
+        return fun
