aefd6d440b7f42163a234f354b845e798bc0c17f
ilan-gold <ilanbassgold@gmail.com>
2022-03-19 16:45:30 -0400

    Fill in functions that might be needed for testing.

--- batchglm/train/numpy/glm_norm/model_container.py
+++ batchglm/train/numpy/glm_norm/model_container.py
@@ -1,3 +1,4 @@
+import math
 from typing import Callable, Union
 
 import dask
@@ -60,19 +61,7 @@ class ModelContainer(BaseModelContainer):
         Scale model jacobian
         :return: observations x features
         """
-        scale = self.scale
-        loc = self.location
-        if isinstance(self.x, scipy.sparse.csr_matrix):
-            scale_plus_x = np.asarray(scale + self.x)
-        else:
-            scale_plus_x = scale + self.x
-        r_plus_mu = scale + loc
-
-        # Define graphs for individual terms of constant term of hessian:
-        const1 = scipy.special.digamma(scale_plus_x) - scipy.special.digamma(scale)
-        const2 = -scale_plus_x / r_plus_mu
-        const3 = np.log(scale) + np.ones_like(scale) - np.log(r_plus_mu)
-        return scale * (const1 + const2 + const3)
+        return -np.ones_like(self.x) - np.power((self.x - self.location) / self.scale, 2)
 
     def jac_weight_scale_j(self, j) -> Union[np.ndarray, dask.array.core.Array]:
         """
@@ -80,22 +69,12 @@ class ModelContainer(BaseModelContainer):
         :param j: Location
         :return: observations x features
         """
-        # Make sure that dimensionality of sliced array is kept:
-        if isinstance(j, int) or isinstance(j, np.int32) or isinstance(j, np.int64):
-            j = [j]
-        scale = self.scale_j(j=j)
-        loc = self.location_j(j=j)
-        if isinstance(self.x, scipy.sparse.csr_matrix):
-            scale_plus_x = np.asarray(scale + self.x[:, j])
-        else:
-            scale_plus_x = scale + self.x[:, j]
-        r_plus_mu = scale + loc
+        return -np.ones_like(self.x[:, j]) - np.power((self.x[:, j] - self.location_j(j=j)) / self.scale_j(j=j), 2)
 
-        # Define graphs for individual terms of constant term of hessian:
-        const1 = scipy.special.digamma(scale_plus_x) - scipy.special.digamma(scale)
-        const2 = -scale_plus_x / r_plus_mu
-        const3 = np.log(scale) + np.ones_like(scale) - np.log(r_plus_mu)
-        return scale * (const1 + const2 + const3)
+    @property
+    def fim_location_location(self):
+        return np.power(self.location / self.scale, 2)
+        
 
     @property
     def fim_location_scale(self) -> np.ndarray:
@@ -134,60 +113,29 @@ class ModelContainer(BaseModelContainer):
         """scale-location block of the hessian matrix"""
         scale = self.scale
         loc = self.location
-        return np.multiply(loc * scale, np.asarray(self.x - loc) / np.square(loc + scale))
+        return (-2 / np.power(scale, 3)) * loc * (self.x - loc)
 
     @property
     def hessian_weight_location_location(self) -> np.ndarray:
         """location-location block of the hessian matrix"""
         scale = self.scale
         loc = self.location
-        if isinstance(self.x, np.ndarray) or isinstance(self.x, dask.array.core.Array):
-            x_by_scale_plus_one = self.x / scale + np.ones_like(scale)
-        else:
-            x_by_scale_plus_one = np.asarray(self.x.divide(scale) + np.ones_like(scale))
-
-        return -loc * x_by_scale_plus_one / np.square((loc / scale) + np.ones_like(loc))
+        return (loc / np.power(scale, 2)) * (self.x - 2 * loc)
 
     @property
     def hessian_weight_scale_scale(self) -> np.ndarray:
         """scale-scale block of the hessian matrix"""
         scale = self.scale
         loc = self.location
-        scale_plus_x = np.asarray(self.x + scale)
-        scale_plus_loc = scale + loc
-        # Define graphs for individual terms of constant term of hessian:
-        const1 = scipy.special.digamma(scale_plus_x) + scale * scipy.special.polygamma(n=1, x=scale_plus_x)
-        const2 = -scipy.special.digamma(scale) + scale * scipy.special.polygamma(n=1, x=scale)
-        const3 = -loc * scale_plus_x + np.ones_like(scale) * 2.0 * scale * scale_plus_loc / np.square(scale_plus_loc)
-        const4 = np.log(scale) + np.ones_like(scale) * 2.0 - np.log(scale_plus_loc)
-        return scale * (const1 + const2 + const3 + const4)
+        return (-2 / np.power(scale, 2)) * np.power(self.x - loc, 2)
 
     @property
     def ll(self) -> Union[np.ndarray, dask.array.core.Array]:
         """log-likelihood"""
         scale = self.scale
         loc = self.location
-        log_r_plus_mu = np.log(scale + loc)
-        if isinstance(self.x, np.ndarray) or isinstance(self.x, dask.array.core.Array):
-            # dense numpy or dask
-            ll = (
-                scipy.special.gammaln(scale + self.x)
-                - scipy.special.gammaln(self.x + np.ones_like(scale))
-                - scipy.special.gammaln(scale)
-                + self.x * (self.eta_loc - log_r_plus_mu)
-                + np.multiply(scale, self.eta_scale - log_r_plus_mu)
-            )
-        else:
-            # sparse scipy
-            ll = (
-                scipy.special.gammaln(np.asarray(scale + self.x))
-                - scipy.special.gammaln(self.x + np.ones_like(scale))
-                - scipy.special.gammaln(scale)
-                + np.asarray(
-                    self.x.multiply(self.eta_loc - log_r_plus_mu) + np.multiply(scale, self.eta_scale - log_r_plus_mu)
-                )
-            )
-            ll = np.asarray(ll)
+        log_scale = np.log(scale)
+        ll = -.5 * np.repeat(2 * math.pi, loc.shape[0])  - log_scale - np.power((self.x - loc) / (2 * scale), 2)
         return self.np_clip_param(ll, "ll")
 
     def ll_j(self, j) -> Union[np.ndarray, dask.array.core.Array]:
@@ -198,33 +146,14 @@ class ModelContainer(BaseModelContainer):
         # Make sure that dimensionality of sliced array is kept:
         if isinstance(j, int) or isinstance(j, np.int32) or isinstance(j, np.int64):
             j = [j]
+
         scale = self.scale_j(j=j)
         loc = self.location_j(j=j)
-        log_r_plus_mu = np.log(scale + loc)
-        if isinstance(self.x, np.ndarray) or isinstance(self.x, dask.array.core.Array):
-            # dense numpy or dask
-            ll = (
-                scipy.special.gammaln(scale + self.x[:, j])
-                - scipy.special.gammaln(self.x[:, j] + np.ones_like(scale))
-                - scipy.special.gammaln(scale)
-                + self.x[:, j] * (self.eta_loc_j(j=j) - log_r_plus_mu)
-                + np.multiply(scale, self.eta_scale_j(j=j) - log_r_plus_mu)
-            )
-        else:
-            # sparse scipy
-            ll = (
-                scipy.special.gammaln(np.asarray(scale + self.x[:, j]))
-                - scipy.special.gammaln(self.x + np.ones_like(scale))
-                - scipy.special.gammaln(scale)
-                + np.asarray(
-                    self.x[:, j].multiply(self.eta_loc_j(j=j) - log_r_plus_mu)
-                    + np.multiply(scale, self.eta_scale_j(j=j) - log_r_plus_mu)
-                )
-            )
-            ll = np.asarray(ll)
+        log_scale = np.log(scale)
+        ll = -.5 * np.repeat(2 * math.pi, loc.shape[0])  - log_scale - np.power((self.x - loc) / (2 * scale), 2)
         return self.np_clip_param(ll, "ll")
 
-    def ll_handle(self) -> Callable:
+    def ll_handle(self) -> Callable: # what?
         def fun(x, eta_loc, theta_scale, xh_scale):
             eta_scale = np.matmul(xh_scale, theta_scale)
             scale = np.exp(eta_scale)
@@ -245,7 +174,7 @@ class ModelContainer(BaseModelContainer):
 
         return fun
 
-    def jac_scale_handle(self) -> Callable:
+    def jac_scale_handle(self) -> Callable: # what?
         def fun(x, eta_loc, theta_scale, xh_scale):
             scale = np.exp(theta_scale)
             loc = np.exp(eta_loc)
