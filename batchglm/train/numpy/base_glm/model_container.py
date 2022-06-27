Fix FIM calculation

batchglm/train/numpy/base_glm/model_container.py
@@ -358,7 +358,7 @@ class NumpyModelContainer(BaseModelContainer):
         fim_scale_scale = self.fim_scale_scale
         fim_location_scale = self.fim_location_scale
         fim_ba = np.transpose(fim_location_scale, axes=[0, 2, 1])
-        return -np.concatenate(
+        return np.concatenate(
             [
                 np.concatenate([fim_location_location, fim_location_scale], axis=2),
                 np.concatenate([fim_ba, fim_scale_scale], axis=2),

batchglm/train/numpy/glm_nb/model_container.py
@@ -18,14 +18,14 @@ class ModelContainer(NumpyModelContainer):
         Fisher inverse matrix weights
         :return: observations x features
         """
-        return -self.location * self.scale / (self.scale + self.location)
+        return self.location * self.scale / (self.scale + self.location)
 
     def fim_weight_location_location_j(self, j) -> Union[np.ndarray, dask.array.core.Array]:
         """
         Fisher inverse matrix weights at j
         :return: observations x features
         """
-        return -self.location_j(j=j) * self.scale_j(j=j) / (self.scale_j(j=j) + self.location_j(j=j))
+        return self.location_j(j=j) * self.scale_j(j=j) / (self.scale_j(j=j) + self.location_j(j=j))
 
     @property
     def jac_weight(self):

batchglm/train/numpy/glm_norm/model_container.py
@@ -38,11 +38,21 @@ class ModelContainer(NumpyModelContainer):
 
     @property
     def fim_location_scale(self) -> np.ndarray:
-        return np.zeros([self.theta_scale.shape[1], 0, 0])
+        return np.zeros([self.model.x.shape[1], self.theta_location.shape[0], self.theta_scale.shape[0]])
 
     @property
-    def fim_scale_scale(self) -> np.ndarray:
-        return np.full([self.theta_scale.shape[1], 0, 0], 2)
+    def fim_weight_scale_scale(self) -> np.ndarray:
+        return np.full(self.scale.shape, 2)
+
+    @property
+    def fim_scale_scale(self) -> Union[np.ndarray, dask.array.core.Array]:
+        """
+
+        :return: (features x inferred param x inferred param)
+        """
+        w = self.fim_weight_scale_scale
+        xh = self.xh_scale
+        return np.einsum("fob,oc->fbc", np.einsum("ob,of->fob", xh, w), xh)
 
     @property
     def hessian_weight_location_scale(self) -> np.ndarray: