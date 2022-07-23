1541d7d7ba2646c3549535f896edac8b0e4d0d05
picciama <mario.picciani@in.tum.de>
2022-07-23 11:10:23 +0200

    added deps for newly included edgeR procedures

--- batchglm/external/edgeR/external.py
+++ batchglm/external/edgeR/external.py
@@ -1,1 +1,7 @@
-from batchglm.train.numpy.base_glm.B.base_glm.model import BaseModelContainer
+from batchglm.models.base_glm import _ModelGLM
+from batchglm.models.glm_nb.model import Model
+from batchglm.models.glm_nb.utils import init_par
+from batchglm.train.numpy.base_glm import BaseModelContainer, EstimatorGlm
+from batchglm.train.numpy.glm_nb import ModelContainer
+
+# from batchglm.train.numpy.glm_nb import Estimator as NBEstimator
