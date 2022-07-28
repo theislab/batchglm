8989469b8f3c6f8c669a3eb16b7d2813341acab2
ilan-gold <ilanbassgold@gmail.com>
2022-07-28 14:49:21 +0200

    Move ll function.

--- batchglm/train/numpy/glm_norm/model_container.py
+++ batchglm/train/numpy/glm_norm/model_container.py
@@ -6,10 +6,12 @@ import numpy as np
 
 from .external import BaseModelContainer
 
+
 def ll(scale, loc, x):
     resid = loc - x
     ll = -.5 * np.log(2 * math.pi) - np.log(scale) - .5 * np.power(resid / scale, 2)
     return ll
+
 
 class ModelContainer(BaseModelContainer):
 
