ce34637a559057d7d87fd32d4bafeffb6995c0e1
picciama <mario.picciani@in.tum.de>
2022-08-06 17:45:54 +0200

    documentation and cleanup

--- batchglm/external/edgeR/qr_decomposition.py
+++ batchglm/external/edgeR/qr_decomposition.py
@@ -6,13 +6,9 @@ from .external import _ModelGLM
 
 class QRDecomposition:
     def __init__(self, nrows: int, ncoefs: int, cur_x):
-
         self.nr = nrows
         self.nc = ncoefs
         self.x = cur_x
-        # self.x_copy = np.zeros((nrows * ncoefs), dtype=float)
-        # self.tau = np.zeros(ncoefs)
-        # self.effects = np.zeros(nrows)
         self.weights = 1.0  # np.zeros(nrows)
         self.lwork_geqrf = -1
         self.lwork_ormqr = -1
@@ -20,13 +16,12 @@ class QRDecomposition:
         self.trans_ormqr = "T"
         self.side = "L"
 
+        # workspace queries calling LAPACK subroutines via scipy.linalg.lapack
         self.x_copy, self.tau, tmpwork, info = dgeqrf(a=np.zeros((self.nr, self.nc)), lwork=self.lwork_geqrf)
         self.lwork_geqrf = tmpwork + 0.5
         if self.lwork_geqrf < 1:
             self.lwork_geqrf = 1
-        # work_geqrf = # TODO work_geqrf.resize(lwork_geqrf);
 
-        # Repeating for dormqr
         self.effects, tmpwork, info = dormqr(
             side=self.side,
             trans=self.trans_ormqr,
@@ -38,7 +33,6 @@ class QRDecomposition:
         self.lwork_ormqr = tmpwork + 0.5
         if self.lwork_ormqr < 1:
             self.lwork_ormqr = 1
-        # work_ormqr = # TODO work_ormqr.resize(lwork_ormqr);
 
     def store_weights(self, w=None):
         if w is None:
@@ -51,7 +45,6 @@ class QRDecomposition:
         self.x_copy *= self.weights
 
         self.x_copy, self.tau, tmpwork, info = dgeqrf(a=self.x_copy, lwork=self.lwork_geqrf)
-        print(self.x_copy.shape, self.tau.shape)
         if info != 0:
             raise RuntimeError("QR decomposition failed")
 
@@ -68,6 +61,13 @@ class QRDecomposition:
 
 
 def get_levenberg_start(model: _ModelGLM, disp: np.ndarray, use_null: bool):
+    """
+    Parameter initialisation of location parameters using QR decomposition.
+    This method is a python version of the C++ code in edgeR.
+    :param model: A GLM model object
+    :param disp: the fixed dispersion parameter used during the calculation.
+    :param use_null: ??? this must be true, the other is not implemented
+    """
 
     n_obs = model.num_observations
     n_features = model.num_features
@@ -99,43 +99,5 @@ def get_levenberg_start(model: _ModelGLM, disp: np.ndarray, use_null: bool):
             output[:, j] = qr.effects[:n_parm, 0]
 
     else:
-        """
-         {
-        const bool weights_are_the_same=allw.is_row_repeated();
-        if (weights_are_the_same && num_tags) {
-            QR.store_weights(allw.get_row(0));
-            QR.decompose();
-        }
-
-        // Finding the delta.
-        double delta=0;
-        if (counts.is_data_integer()) {
-            Rcpp::IntegerMatrix imat=counts.get_raw_int();
-            delta=*std::max_element(imat.begin(), imat.end());
-        } else {
-            Rcpp::NumericMatrix dmat=counts.get_raw_dbl();
-            delta=*std::max_element(dmat.begin(), dmat.end());
-        }
-        delta=std::min(delta, 1.0/6);
-
-        for (int tag=0; tag<num_tags; ++tag) {
-            if (!weights_are_the_same) {
-                QR.store_weights(allw.get_row(tag));
-                QR.decompose();
-            }
-            counts.fill_row(tag, current.data());
-            const double* optr=allo.get_row(tag);
-
-            // Computing normalized log-expression values.
-            for (int lib=0; lib<num_libs; ++lib) {
-                current[lib]=std::log(std::max(delta, current[lib])) - optr[lib];
-            }
-
-            // Performing the QR decomposition and taking the solution.
-            QR.solve(current.data());
-            auto curout=output.row(tag);
-            std::copy(QR.effects.begin(), QR.effects.begin()+num_coefs, curout.begin());
-        }
-        """
         raise NotImplementedError("This method is not yet implemented.")
     return output
