5d54b1f2aaae4d8d09ebd117df571619b9014dfd
picciama <mario.picciani@in.tum.de>
2022-08-06 17:53:56 +0200

    cleanup / bugfixing /integration in estimator

--- batchglm/external/edgeR/glm_one_group.py
+++ batchglm/external/edgeR/glm_one_group.py
@@ -1,55 +1,67 @@
 import logging
+from typing import Optional, Union
 
 import numpy as np
 
-logger = logging.get_logger(__name__)
+from .external import BaseModelContainer
+
+low_value = 1e-10
+logger = logging.getLogger(__name__)
 
 
-def fit(
-    data: np.ndarray,
-    size_factors: np.ndarray,
-    dispersion: float,
-    weights: np.ndarray = 1.0,
+def get_single_group_start(
+    x: np.ndarray,
+    sf: Optional[np.ndarray] = None,
+    weights: Optional[Union[np.ndarray, float]] = None,
+):
+    if weights is None:
+        weights = np.ones_like(x)
+    if weights.shape != x.shape:
+        raise ValueError("Shape of weights must be idential to shape of model.x")
+
+    total_weights = weights.sum(axis=0)
+
+    if sf is None:
+        sf = np.log(1.0)
+
+    theta_location = np.sum(np.where(x > low_value, x / np.exp(sf) * weights, 0), axis=0, keepdims=True)
+    theta_location = np.log(theta_location / total_weights)
+    return theta_location
+
+
+def fit_single_group(
+    model: BaseModelContainer,
     maxit: int = 50,
     tolerance: int = 1e-10,
-    cur_beta: np.ndarray = None,
 ):
     """
     Setting up initial values for beta as the log of the mean of the ratio of counts to offsets.
     * This is the exact solution for the gamma distribution (which is the limit of the NB as
     * the dispersion goes to infinity. However, if cur_beta is not NA, then we assume it's good.
     """
-    low_value = 10 ** -10
-    low_mask = data > low_value
-    # nonzero = np.any(low_mask, axis=0)
-    if cur_beta is None:
-        cur_beta = np.zeros(size_factors.shape[0], dtype=float)
-        total_weight = np.zeros_like(cur_beta)
+    low_mask = np.all(model.x <= low_value, axis=0).compute()
+    unconverged_idx = np.where(~low_mask)[0]
 
-        cur_beta = np.sum(data / np.exp(size_factors) * weights * low_mask, axis=0)
-        total_weight = weights * size_factors.shape[0]
-        cur_beta = np.log(cur_beta / total_weight)
+    iteration = 0
+    weights = 1.0
 
-    # Skipping to a result for all-zero rows.
-    # if (!nonzero) {
-    # return std::make_pair(R_NegInf, true);
+    step = np.zeros((1, model.num_features), dtype=float)
 
-    # // Newton-Raphson iterations to converge to mean.
-    has_converged = np.zeros(data.shape[1], dtype=bool)
-    for _ in range(maxit):
-        # dl = np.zeros(data.shape[1], dtype=float)
-        # info = np.zeros_like(dl)
-        mu = np.exp(cur_beta + size_factors)
-        denominator = 1 + mu * dispersion
-        dl = np.sum((data - mu) / denominator * weights, axis=0)
-        info = np.sum(mu / denominator * weights, axis=0)
-        step = dl / info
-        step = np.where(has_converged, 0.0, step)
-        cur_beta += step
-        has_converged = np.abs(step) < tolerance
-        if np.all(has_converged):
+    while iteration < maxit:
+        loc_j = model.location_j(unconverged_idx)
+        scale_j = 1 / model.scale_j(unconverged_idx)
+        denominator = 1 + loc_j * scale_j
+
+        dl = np.sum((model.x[:, unconverged_idx] - loc_j) / denominator * weights, axis=0).compute()
+
+        info = np.sum(loc_j / denominator * weights, axis=0).compute()
+        cur_step = dl / info
+        step[0, unconverged_idx] = cur_step
+        model.theta_location = model.theta_location.compute() + step
+        unconverged_idx = unconverged_idx[np.abs(cur_step) >= tolerance]
+        if len(unconverged_idx) == 0:
             break
+        step.fill(0)
+        iteration += 1
     else:
         logger.warning("Maximum iterations exceeded.")
-
-    return cur_beta, has_converged
