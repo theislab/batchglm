8c4e96cd995d7229acc578071720f8042d15d173
picciama <mario.picciani@in.tum.de>
2022-08-06 17:54:31 +0200

    added imports

--- batchglm/external/edgeR/external.py
+++ batchglm/external/edgeR/external.py
@@ -1,7 +1,8 @@
 from batchglm.models.base_glm import _ModelGLM
-from batchglm.models.glm_nb.model import Model
+from batchglm.models.glm_nb.model import Model as NBModel
 from batchglm.models.glm_nb.utils import init_par
 from batchglm.train.numpy.base_glm import BaseModelContainer, EstimatorGlm
 from batchglm.train.numpy.glm_nb import ModelContainer
+from batchglm.utils.input import InputDataGLM
 
 # from batchglm.train.numpy.glm_nb import Estimator as NBEstimator
