515800e74cd36e33f093f36065bf59a69c5a6948
ilan-gold <ilanbassgold@gmail.com>
2022-08-08 17:51:54 +0200

    Fixes.

--- batchglm/models/glm_poisson/model.py
+++ batchglm/models/glm_poisson/model.py
@@ -4,10 +4,10 @@ from typing import Any, Callable, Dict, Optional, Tuple, Union
 import dask.array
 import numpy as np
 
-from .external import _ModelGLM
+from .external import ModelGLM
 
 
-class Model(_ModelGLM, metaclass=abc.ABCMeta):
+class Model(ModelGLM, metaclass=abc.ABCMeta):
     """
     Generalized Linear Model (GLM) with Poisson noise.
     """
@@ -69,7 +69,7 @@ class Model(_ModelGLM, metaclass=abc.ABCMeta):
             "loc": np.nextafter(dmax, -np.inf, dtype=dtype) / sf,
             "scale": np.nextafter(dmax, -np.inf, dtype=dtype) / sf,
             "likelihood": dtype(1),
-            "ll": dtype(0),
+            "ll": dtype(10000), # poisson models can have large log likelhoods initially
             # Not used and should be removed: https://github.com/theislab/batchglm/issues/148
             "theta_scale": np.log(dmax) / sf,
             "eta_scale": np.log(dmax) / sf,
@@ -85,7 +85,7 @@ class Model(_ModelGLM, metaclass=abc.ABCMeta):
 
     @property
     def rand_fn(self) -> Optional[Callable]:
-        return lambda shape: np.abs(np.random.uniform(0.5, 2, shape))
+        return lambda shape: np.abs(np.random.uniform(2, 10, shape))
 
     @property
     def rand_fn_loc(self) -> Optional[Callable]:
--- batchglm/train/numpy/glm_poisson/model_container.py
+++ batchglm/train/numpy/glm_poisson/model_container.py
@@ -5,10 +5,10 @@ import numpy as np
 import scipy
 
 from .exceptions import NoScaleError
-from .external import BaseModelContainer
+from .external import NumpyModelContainer
 
 
-class ModelContainer(BaseModelContainer):
+class ModelContainer(NumpyModelContainer):
     @property
     def fim_weight_location_location(self) -> Union[np.ndarray, dask.array.core.Array]:
         """
@@ -105,3 +105,15 @@ class ModelContainer(BaseModelContainer):
     @property
     def jac_weight_scale_j(self) -> np.ndarray:
         raise NoScaleError("jac_weight_scale_j")
+
+    @property
+    def hessian(self) -> Union[np.ndarray, dask.array.core.Array]:
+        return self.hessian_location_location
+
+    @property
+    def fim(self) -> Union[np.ndarray, dask.array.core.Array]:
+        return self.fim_location_location
+
+    @property
+    def jac(self) -> Union[np.ndarray, dask.array.core.Array]:
+        return self.jac_location
