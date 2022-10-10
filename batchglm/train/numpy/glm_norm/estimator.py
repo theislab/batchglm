f752db6c55e614ba0df36fb64470168e7161427d
ilan-gold <ilanbassgold@gmail.com>
2022-10-10 17:13:14 -0400

    (fix): loc not scale design matrix

--- batchglm/train/numpy/glm_norm/estimator.py
+++ batchglm/train/numpy/glm_norm/estimator.py
@@ -68,7 +68,7 @@ class Estimator(EstimatorGlm):
     ):
         model = self._model_container.model
         if self._train_loc:
-            theta_location, _, _, _ = np.linalg.lstsq(model.xh_scale, model.x)
+            theta_location, _, _, _ = np.linalg.lstsq(model.xh_loc, model.x)
             self._model_container.theta_location = theta_location
         self._train_loc = False
         super().train(**kwargs)
